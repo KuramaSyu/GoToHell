4e9d2c55f61a955f288610fff0105e37d3da3f60
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-03-05 21:18:01 +0100

    chore: Use secondary color for title

--- src/frontend/src/components/TopBar.tsx
+++ src/frontend/src/components/TopBar.tsx
@@ -4,15 +4,19 @@ import Typography from '@mui/material/Typography';
 import Toolbar from '@mui/material/Toolbar';
 import DiscordLogin from "./DiscordLogin";
 import Box from "@mui/material/Box";
+import { useThemeStore } from "../useThemeStore";
+
 
 const TopBar: React.FC = () => {
+    const {currentTheme, setTheme} = useThemeStore();
+
     return (
       <AppBar position="fixed">
         <Toolbar>
           <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'flex-start' }}>
-            <Typography variant="h2" component="div">
-              Go To Hell
-            </Typography>
+          <Typography variant="h2" component="div" sx={{ color: 'secondary.main' }}>
+            Go To Hell
+          </Typography>
           </Box>
           <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
             <DiscordLogin />
