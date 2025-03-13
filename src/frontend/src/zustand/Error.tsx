0f962e224d9ca1791dfa26f645fd9a80e74ce2f3
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-03-13 20:45:24 +0100

    Create Error.tsx

---
+++ src/frontend/src/zustand/Error.tsx
@@ -0,0 +1,13 @@
+import {create} from 'zustand';
+
+interface AppState {
+    errorMessage: string;
+    setErrorMessage: (message: string) => void;
+}
+
+const useAppState = create<AppState>((set) => ({
+    errorMessage: '',
+    setErrorMessage: (message) => set({ errorMessage: message}),
+}));
+
+export default useAppState
