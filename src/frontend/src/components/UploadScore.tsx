9ca1ac79eb6e11471b3338aca6646ecdd5122017
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-03-13 20:45:01 +0100

    ui: better error handling of button

--- src/frontend/src/components/UploadScore.tsx
+++ src/frontend/src/components/UploadScore.tsx
@@ -4,9 +4,12 @@ import { ThemeProvider, CssBaseline, Button, Container, Box, Typography } from '
 import { darkTheme, nordTheme, themes } from '../themes';
 import { useThemeStore } from '../useThemeStore';
 import { darken } from '@mui/material/styles';
-import { Sport, useSportStore } from '../useSportStore';
+import { SportDefinition, useSportStore } from '../useSportStore';
 import { useDeathAmountState } from "./SportSelect";
 import SendIcon from '@mui/icons-material/Send';
+import { useUserStore } from "../userStore";
+import SportRow from "../models/Sport";
+import useAppState from "../zustand/Error";
 
 
 const map = new Map();
@@ -16,14 +19,36 @@ map.set("plank", "Seconds Plank")
 export const UploadScore = () => {
     const {currentSport} = useSportStore();
     const {amount} = useDeathAmountState();
+    const {user} = useUserStore();
+    const {setErrorMessage} = useAppState();
 
-    if (!currentSport) {
-        // return <Button color="error" size="large">No Sport selected</Button> 
+    const OnUploadClick = () => {
+        if (!currentSport) {
+            return setErrorMessage("Please select a Sport first") 
+        }
+
+        if (amount == 0) {
+            return setErrorMessage("Yeah, just upload nothing. Good idea indeed")
+        }
+        const handlePostRequest = async () => {
+            const sport = new SportRow(currentSport.kind!, currentSport.game!, computedValue)
+            console.timeLog(`Upload Sport: ${sport.toJson()}`)
+            const fut = sport.upload()
+        }
+
+    }
+    if (!(currentSport && user)) {
+        //setErrorMessage("Please Login")
         return (<Box></Box>)
     }
     const computedValue = currentSport.death_multiplier * amount;
     return (
-        <Button sx={{px: 8, py: 3}} variant="outlined" endIcon={<SendIcon fontWeight="large" />}>
+        <Button 
+        sx={{px: 8, py: 3}} 
+        variant="outlined" 
+        endIcon={<SendIcon fontWeight="large" />} 
+            onClick={OnUploadClick}
+        >
             <Typography variant="h4" fontWeight="bold">Upload</Typography>
         </Button> 
     )
