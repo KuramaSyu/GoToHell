f505dcb90def8405b6123fa3f349cd64d49f12ae
kurama <paul.zenker.pz@gmail.com>
2025-03-15 21:19:45 +0100

    chore: removed comment

--- src/frontend/src/theme/themeAPI.ts
+++ src/frontend/src/theme/themeAPI.ts
@@ -1,19 +1,4 @@
 import { ThemeManager } from './themeManager';
-import { customThemes, useThemeStore } from '../zustand/useThemeStore';
-import { CustomThemeConfig } from './interfaces';
+import { customThemes } from '../zustand/useThemeStore';
 
 const themeManager = new ThemeManager(customThemes);
-
-/**
- * Asynchronously sets the current theme by name.
- * It always executes Vibrant extraction on the background image,
- * then generates a full theme (including all vibrant swatches) and updates the zustand store.
- */
-// export async function setCustomTheme(themeName: string): Promise<void> {
-//   const newTheme = await themeManager.generateTheme(themeName);
-//   if (newTheme) {
-//     useThemeStore.getState().setTheme(newTheme);
-//   } else {
-//     console.error(`Unable to set theme: ${themeName}`);
-//   }
-// }
