0246df0fea66dfd54ea6ec3dc0874d66285b713f
kurama <paul.zenker.pz@gmail.com>
2025-03-15 12:42:51 +0100

    refactor: moved SportSelect to own file

--- src/frontend/src/components/GameSelect.tsx
+++ src/frontend/src/components/GameSelect.tsx
@@ -19,67 +19,6 @@ const sportIconMap: Record<string, string> = {
 	plank: plankSVG,
 };
 
-// Select the sport kind with a button
-export const SportSelector = () => {
-	const { currentTheme } = useThemeStore();
-	const { currentSport, setSport } = useSportStore();
-	const [apiData, setApiData] = useState<{ data: SportDefinition[] } | null>(
-		null
-	);
-
-	// Fetch data from /api/default on localhost:8080
-	useEffect(() => {
-		fetch(`${BACKEND_BASE}/api/default`)
-			.then((response) => response.json())
-			.then((data: { data: SportDefinition[] }) => setApiData(data))
-			.catch(console.error);
-	}, []);
-	if (apiData == null) {
-		return <Typography>Waiting for Gin</Typography>;
-	}
-
-	// change sport, when game is changed
-	if (currentTheme != currentSport?.game) {
-		const matchingSport = apiData.data.find(
-			(sport) =>
-				sport.game === currentTheme && sport.kind == currentSport?.kind
-		);
-		if (matchingSport) {
-			setSport(matchingSport);
-		}
-	}
-	console.log(apiData);
-	return (
-		<Box>
-			{/* Vertical ButtonGroup for sports selection */}
-			<ButtonGroup orientation="vertical" fullWidth>
-				{apiData.data
-					.filter((sport) => sport.game === currentTheme)
-					.map((sport) => {
-						return (
-							<Button
-								onClick={() => setSport(sport)}
-								variant={
-									sport.kind === currentSport?.kind
-										? 'contained'
-										: 'outlined'
-								}
-								key={sport.kind}
-							>
-								<img
-									src={sportIconMap[sport.kind]}
-									alt={sport.kind}
-									style={{ width: 50, height: 50 }}
-								/>
-								<Typography>{sport.kind}</Typography>
-							</Button>
-						);
-					})}
-			</ButtonGroup>
-		</Box>
-	);
-};
-
 export const GameSelector = () => {
 	const { currentTheme, setTheme } = useThemeStore();
 	const validGames = ['league', 'overwatch'];
