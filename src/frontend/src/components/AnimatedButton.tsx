54a3e248e3a75da5f0f584ee2505223db27b322b
kurama <paul.zenker.pz@gmail.com>
2025-03-16 12:37:25 +0100

    Create AnimatedButton.tsx

---
+++ src/frontend/src/components/AnimatedButton.tsx
@@ -0,0 +1,60 @@
+import { Button, useTheme } from '@mui/material';
+import { motion } from 'framer-motion';
+import { styled } from '@mui/system';
+import { ReactNode } from 'react';
+
+interface AnimatedButtonProps {
+  children: ReactNode;
+  onClick?: () => void;
+  duration?: number; // new prop added to control animation duration
+}
+
+const AnimatedButton = ({
+  children,
+  onClick,
+  duration = 10,
+}: AnimatedButtonProps) => {
+  const theme = useTheme();
+
+  const isAnimationActive = duration !== 0;
+
+  const AnimatedBtn = styled(motion(Button))({
+    position: 'relative',
+    overflow: 'hidden',
+    borderWidth: '3px', // increased outline width
+    color: theme.palette.common.white,
+    fontWeight: 'bold',
+    background: isAnimationActive
+      ? `linear-gradient(90deg, 
+          ${theme.palette.secondary.light}, 
+          ${theme.palette.secondary.main}, 
+          ${theme.palette.secondary.dark}, 
+          ${theme.palette.secondary.main},
+          ${theme.palette.primary.light}, 
+          ${theme.palette.primary.main}, 
+          ${theme.palette.primary.dark}, 
+          ${theme.palette.primary.main}
+          )`
+      : 'transparent',
+    backgroundSize: isAnimationActive ? '900% 100%' : '100% 100%',
+  });
+
+  return (
+    <AnimatedBtn
+      {...(isAnimationActive
+        ? {
+            animate: {
+              backgroundPosition: ['0% 50%', '100% 50%', '200% 50%', '0% 50%'],
+            },
+            transition: { duration, repeat: Infinity, ease: 'easeInOut' },
+          }
+        : {})}
+      variant={isAnimationActive ? 'contained' : 'outlined'}
+      onClick={onClick}
+    >
+      {children}
+    </AnimatedBtn>
+  );
+};
+
+export default AnimatedButton;
