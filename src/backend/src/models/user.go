861095b79a0cd7a5861fc0c367d59a73c1511809
kurama <paul.zenker.pz@gmail.com>
2025-03-19 20:57:15 +0100

    api: added JsUser to represent user with string id

--- src/backend/src/models/user.go
+++ src/backend/src/models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // Discord User Representation
 type User struct {
@@ -14,4 +17,36 @@ type User struct {
 // GetAvatarURL returns the user's Discord avatar URL
 func (u *User) GetAvatarURL() string {
 	return fmt.Sprintf("https://cdn.discordapp.com/avatars/%v/%v.png", u.ID, u.Avatar)
+}
+func (s *User) ParseJS() JsUser {
+
+	return JsUser{
+		ID:            fmt.Sprint(s.ID),
+		Username:      s.Username,
+		Discriminator: s.Discriminator,
+		Avatar:        s.Avatar,
+		Email:         s.Email,
+	}
+}
+
+type JsUser struct {
+	ID            string `json:"id"`
+	Username      string `json:"username"`
+	Discriminator string `json:"discriminator"`
+	Avatar        string `json:"avatar"`
+	Email         string `json:"email"`
+}
+
+func (s *JsUser) Parse() (*User, error) {
+	id, err := strconv.Atoi(s.ID)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:            uint(id),
+		Username:      s.Username,
+		Discriminator: s.Discriminator,
+		Avatar:        s.Avatar,
+		Email:         s.Email,
+	}, nil
 }
