6666688dcf6ffc9a9e07a9a9c64ca78b6b368f4f
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-03-22 20:33:29 +0100

    chore: added themeprovider on toplevel

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -4,21 +4,26 @@ import './styles/DiscordLogin.css';
 import '@mui/material/styles';
 //import './App.css';
 import HomePage from './components/HomePage';
-import FriendOverview from './components/friendOverview';
+import FriendOverview from '../pages/friends/FriendOverview';
 import TopBar from './components/TopBar';
-
+import {CssBaseline, ThemeProvider} from "@mui/material"
+import { useThemeStore } from './zustand/useThemeStore';
 // Import other components as needed
 
 const App: React.FC = () => {
+  const {theme} = useThemeStore();
   return (
     <Router>
-      <TopBar></TopBar>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login-success" element={<HomePage />} />
-        <Route path="/friends" element={<FriendOverview />}/>
-        {/* Add other routes as needed */}
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <TopBar></TopBar>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login-success" element={<HomePage />} />
+          <Route path="/friends" element={<FriendOverview />}/>
+          {/* Add other routes as needed */}
+        </Routes>
+
+      </ThemeProvider>
     </Router>
   );
 };
