5a00e1e5b79756d96a6c82a62ab66b1513ce5837
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-03-26 21:03:50 +0100

    chore: cleanup

--- src/frontend/src/components/RecentSports/TabView.tsx
+++ src/frontend/src/components/RecentSports/TabView.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Tabs, Tab, Box } from '@mui/material';
 import { RecentSportsStandard } from './Standard';
-import { HorizontalSportsTimeline } from './Timeline';
 
 export const RecentSports = () => {
   const [activeTab, setActiveTab] = useState(0);
