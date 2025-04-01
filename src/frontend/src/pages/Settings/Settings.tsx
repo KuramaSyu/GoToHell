8e83192e63e260738a9ba8866a61d3d619205f10
kurama <paul.zenker.pz@gmail.com>
2025-04-01 16:02:23 +0200

    Update Settings.tsx

--- src/frontend/src/pages/Settings/Settings.tsx
+++ src/frontend/src/pages/Settings/Settings.tsx
@@ -1,6 +1,27 @@
-import { Box } from '@mui/material';
-import React from 'react';
+import { Box, Grid2 as Grid } from '@mui/material';
+import React, { useState } from 'react';
+import { UserPreferences } from './PreferencesInterface';
 
 export const Settings: React.FC = () => {
-  return <Box>test</Box>;
+  const [preferences, setPreferences] = useState<UserPreferences>({
+    game_overrides: [],
+    multipliers: [],
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<{ name?: string; value: unknown }>
+  ) => {
+    const { name, value } = event.target;
+    setPreferences({
+      ...preferences,
+      [name as string]: value,
+    });
+  };
+  return (
+    <Box>
+      test
+      {/* Grid with multipliers */}
+      {/* Grid with game overrides */}
+    </Box>
+  );
 };
