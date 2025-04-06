f7d0c7cabc28efa692439d1f3a126e5f83eba650
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-04-06 13:08:15 +0200

    ui,feat: adding/removing overrides

--- src/frontend/src/pages/Settings/Settings.tsx
+++ src/frontend/src/pages/Settings/Settings.tsx
@@ -9,7 +9,7 @@ import {
   Select,
   TextField,
 } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserPreferences } from '../../models/Preferences';
 import usePreferenceStore from '../../zustand/PreferenceStore';
 import { getThemeNames } from '../../zustand/useThemeStore';
@@ -17,6 +17,8 @@ import { useSportResponseStore } from '../../zustand/sportResponseStore';
 import AppBackground from '../../components/AppBackground';
 import { GetSportsResponse } from '../../models/Sport';
 import useAppState from '../../zustand/Error';
+import { Add, Remove } from '@mui/icons-material';
+import { getCookie, setCookie } from '../../utils/cookies';
 
 type CustomSelectProps = {
   items: Record<string, string>;
@@ -56,26 +58,223 @@ export const MultiplierSettings: React.FC = () => {
   return <Box sx={{ display: 'flex', flexDirection: 'row' }}></Box>;
 };
 
+type GameOverrideProps = {
+  game: string;
+  sport: string;
+  amount: number;
+};
+
+export const GameOverrideViewmodel: React.FC<GameOverrideProps> = ({
+  game,
+  sport,
+  amount,
+}) => {
+  const { setErrorMessage } = useAppState();
+  const { preferences, setPreferences } = usePreferenceStore();
+
+  const removeItem = (game: string, sport: string, amount: number) => {
+    const index = preferences.game_overrides.findIndex(
+      (item) =>
+        item.game === game && item.sport === sport && item.amount === amount
+    );
+    if (index !== -1) {
+      preferences.game_overrides.splice(index, 1);
+      setCookie('preferences', JSON.stringify(preferences), 999);
+      setPreferences(preferences);
+    } else {
+      setErrorMessage(
+        'Could not find the game override to remove. Please try again.'
+      );
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 5,
+        padding: 2,
+        borderRadius: 5,
+        backgroundColor: 'rgba(0,0,0,0.2)',
+      }}
+    >
+      <Box
+        sx={{
+          flex: 1,
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      >
+        {game}
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      >
+        {sport}
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      >
+        {amount}
+      </Box>
+      <Button
+        variant="contained"
+        sx={{ flex: '0 1 auto', maxWidth: 1 / 10, borderRadius: 10 }}
+        onClick={() => removeItem(game, sport, amount)}
+      >
+        <Remove></Remove>
+      </Button>
+    </Box>
+  );
+};
+
+export const GameOverrideList: React.FC = () => {
+  const { preferences } = usePreferenceStore();
+  const { sportResponse } = useSportResponseStore();
+
+  const rows = preferences.game_overrides.map((gameOverride) => {
+    return (
+      <GameOverrideViewmodel
+        key={gameOverride.game + gameOverride.sport}
+        game={gameOverride.game}
+        sport={gameOverride.sport}
+        amount={gameOverride.amount}
+      ></GameOverrideViewmodel>
+    );
+  });
+
+  const TableHeader = (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 5,
+        padding: 2,
+        borderRadius: 5,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+      }}
+    >
+      {' '}
+      <Box
+        sx={{
+          flex: 1,
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      >
+        Game
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      >
+        Sport
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      >
+        Exercises per death
+      </Box>
+      <Button
+        variant="contained"
+        sx={{
+          flex: '0 1 auto',
+          maxWidth: 1 / 10,
+          borderRadius: 10,
+          visibility: 'hidden',
+        }}
+      >
+        <Remove></Remove>
+      </Button>
+    </Box>
+  );
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        backdropFilter: 'blur(25px)',
+        p: 2,
+        borderRadius: 5,
+        gap: 1,
+      }}
+    >
+      {TableHeader}
+      {rows}
+    </Box>
+  );
+};
+
 export const GameOverrideSettings: React.FC = () => {
   const { preferences, setPreferences } = usePreferenceStore();
   const { sportResponse, setSportResponse } = useSportResponseStore();
   const [game, setGame] = useState<string | null>(null);
   const [sport, setSport] = useState<string | null>(null);
+  const [exerciseAmount, setExerciseAmount] = useState<number>(0);
   const menuItems = getThemeNames();
   const { setErrorMessage } = useAppState();
-  const add = () => {};
-  if (sportResponse == null) {
-    return null;
+
+  const add = () => {
+    if (game === null) {
+      return setErrorMessage('You need to select a game');
+    }
+
+    if (sport === null) {
+      return setErrorMessage('You need to select a sport');
+    }
+
+    if (exerciseAmount === null) {
+      return setErrorMessage('Well, how many exerpices per death?');
+    }
+    preferences.game_overrides.push({
+      game: game,
+      sport: sport,
+      amount: exerciseAmount,
+    });
+    setCookie('preferences', JSON.stringify(preferences), 999);
+    setPreferences(preferences);
+  };
+
+  if (!sportResponse || !sportResponse.sports) {
+    return <Box sx={{ textAlign: 'center', p: 2 }}>Loading sports data...</Box>;
   }
 
+  // make a map[str: str] for the sports
   const sports = sportResponse.sports
     ? Object.keys(sportResponse.sports).reduce((prev, current) => {
         prev[current] = current;
         return prev;
       }, {} as Record<string, string>)
     : {};
+
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'row', gap: 5 }}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 5,
+        p: 5,
+        borderRadius: 5,
+        backgroundColor: 'rgba(0,0,0,0.2)',
+        backdropFilter: 'blur(25px)',
+      }}
+    >
       <CustomSelect
         items={getThemeNames().reduce((prev, current) => {
           prev[current] = current;
@@ -94,16 +293,24 @@ export const GameOverrideSettings: React.FC = () => {
       <TextField
         sx={{ flex: 1 }}
         variant="outlined"
-        required={true}
         type="number"
         label="Exercises per death"
+        value={exerciseAmount}
+        onChange={(e) => {
+          const value = parseInt(e.target.value, 10);
+          if (!isNaN(value) && value >= 0) {
+            setExerciseAmount(Number(value));
+          } else {
+            setExerciseAmount(0);
+          }
+        }}
       ></TextField>
       <Button
         onClick={add}
         variant="contained"
-        sx={{ flex: '0 1 auto', width: 1 / 8 }}
+        sx={{ flex: '0 1 auto', maxWidth: 1 / 10 }}
       >
-        Add
+        <Add />
       </Button>
     </Box>
   );
@@ -112,15 +319,14 @@ export const GameOverrideSettings: React.FC = () => {
 export const Settings: React.FC = () => {
   const { preferences, setPreferences } = usePreferenceStore();
 
-  const handleChange = (
-    event: React.ChangeEvent<{ name?: string; value: unknown }>
-  ) => {
-    const { name, value } = event.target;
-    setPreferences({
-      ...preferences,
-      [name as string]: value,
-    });
-  };
+  useEffect(() => {
+    // read preferences on page load
+    const value = getCookie('preferences');
+    if (value != null) {
+      setPreferences(JSON.parse(value));
+    }
+  }, []);
+
   return (
     <>
       <AppBackground></AppBackground>
@@ -133,11 +339,29 @@ export const Settings: React.FC = () => {
           height: '100%',
           justifyContent: 'center',
           alignItems: 'center',
+          gap: 5,
         }}
       >
         {/* Grid with multipliers */}
-        <Box sx={{ width: 4 / 5, flex: '0 1 auto', justifyItems: 'center' }}>
+        <Box
+          sx={{
+            width: 4 / 5,
+            flex: '0 1 auto',
+            justifyItems: 'center',
+            zIndex: 1,
+          }}
+        >
           <GameOverrideSettings />
+        </Box>
+        <Box
+          sx={{
+            width: 4 / 5,
+            flex: '0 1 auto',
+            justifyItems: 'center',
+            zIndex: 1,
+          }}
+        >
+          <GameOverrideList></GameOverrideList>
         </Box>
         {/* Grid with game overrides */}
         <MultiplierSettings />
