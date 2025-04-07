b7204fb303ecadfb4207727477c3035084d7ab9e
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-04-07 18:02:29 +0200

    ui: better alignment

--- src/frontend/src/pages/Settings/Settings.tsx
+++ src/frontend/src/pages/Settings/Settings.tsx
@@ -26,40 +26,43 @@ export const Settings: React.FC = () => {
           height: '100%',
           justifyContent: 'center',
           alignItems: 'center',
-          gap: 5,
+          gap: 10,
         }}
       >
         {/* Grid with overrides */}
-        <Typography variant="h2" sx={{ zIndex: 1 }}>
-          Overrides
-        </Typography>
+
         <Box
           sx={{
             width: 4 / 5,
             flex: '0 1 auto',
             justifyItems: 'center',
             zIndex: 1,
-            backdropFilter: 'blur(25px)',
-            borderRadius: 5,
-            gap: 5,
-            py: 5,
+
             display: 'flex',
             flexDirection: 'column',
           }}
         >
-          <GameOverrideSettings />
-          <GameOverrideList />
+          <Typography variant="h2" sx={{ zIndex: 1 }}>
+            Overrides
+          </Typography>
+          <Box
+            sx={{
+              flex: '0 1 auto',
+              justifyItems: 'center',
+              backdropFilter: 'blur(25px)',
+              borderRadius: 5,
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 5,
+            }}
+          >
+            <GameOverrideSettings />
+            <GameOverrideList />
+          </Box>
         </Box>
-        <Box
-          sx={{
-            width: 4 / 5,
-            flex: '0 1 auto',
-            justifyItems: 'center',
-            zIndex: 1,
-          }}
-        ></Box>
 
         {/* Grid with game multipliers */}
+
         <Box
           sx={{
             width: 4 / 5,
@@ -68,7 +71,9 @@ export const Settings: React.FC = () => {
             zIndex: 1,
           }}
         >
-          <Typography variant="h2">Multipliers</Typography>
+          <Typography variant="h2" sx={{ zIndex: 1 }}>
+            Multipliers
+          </Typography>
           <MultiplierSettings />
         </Box>
       </Box>
