f59751f09e87ac243c5fcf1811c114fff1f6fa4c
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-04-16 17:00:29 +0200

    api: added and implemented fetchFriends()

--- src/frontend/src/utils/api/Api.ts
+++ src/frontend/src/utils/api/Api.ts
@@ -3,12 +3,14 @@ import { SportScore } from '../../models/Sport';
 import { BACKEND_BASE } from '../../statics';
 import { useTotalScoreStore } from '../../zustand/TotalScoreStore';
 import { DiscordUser, DiscordUserImpl } from '../../components/DiscordLogin';
-import { useUserStore } from '../../userStore';
+import { useUsersStore, useUserStore } from '../../userStore';
+import { FriendshipReply } from '../../pages/friends/FriendOverview';
 
 export interface BackendApiInterface {}
 export interface UserApiInterface {
   fetchTotalScore(): Promise<Response>;
   fetchUser(): Promise<Response>;
+  fetchFriends(): Promise<FriendshipReply | null>;
 }
 
 // Class, to fetch resources from the backend. Responses will be
@@ -17,8 +19,11 @@ export class DefaultBackendApi implements BackendApiInterface {}
 
 // represents the backend methods, which are needed for user purposes
 export class UserApi implements UserApiInterface {
-  logError(url: string, error: any): void {
-    console.error(`Error fetching ${url}:`, JSON.stringify(error));
+  logError(url_part: string, error: any): void {
+    console.error(
+      `Error fetching ${BACKEND_BASE}${url_part}:`,
+      JSON.stringify(error)
+    );
   }
 
   /**
@@ -38,7 +43,7 @@ export class UserApi implements UserApiInterface {
         setAmounts(parsed_data.results);
       }
     } else {
-      this.logError(`${BACKEND_BASE}/api/sports/total`, fut.json());
+      this.logError(`/api/sports/total`, fut.json());
     }
     return fut;
   }
@@ -57,8 +62,33 @@ export class UserApi implements UserApiInterface {
       const userData: DiscordUser = await response.json();
       setUser(new DiscordUserImpl(userData));
     } else {
-      this.logError(`${BACKEND_BASE}/api/auth/user`, response.json());
+      this.logError(`/api/auth/user`, response.json());
     }
     return response;
+  }
+
+  /**
+   * This fetches all friendships and adds these users to the useUsersStore.
+   * It returns the friendshipreply
+   */
+  async fetchFriends(): Promise<FriendshipReply | null> {
+    const API_ENDPOINT = '/api/friends';
+    const response = await fetch(`${BACKEND_BASE}${API_ENDPOINT}`, {
+      credentials: 'include',
+    });
+    const result = await response.json();
+    if (response.ok) {
+      // set zustand store and return reply
+      const addUser = useUsersStore.getState().addUser;
+      const reply = result['data'] as FriendshipReply;
+      reply.users.forEach((friend) => {
+        addUser(new DiscordUserImpl(friend));
+      });
+      return reply;
+    } else {
+      // log error and return null
+      this.logError(API_ENDPOINT, await result.json());
+    }
+    return null;
   }
 }
