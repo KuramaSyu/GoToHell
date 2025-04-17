5b95de7b77824bb6fb60a3de9cb8b90703dd4960
kurama <paul.zenker.pz@gmail.com>
2025-04-17 18:26:53 +0200

    api: streak can be null

--- src/frontend/src/zustand/StreakStore.tsx
+++ src/frontend/src/zustand/StreakStore.tsx
@@ -1,11 +1,11 @@
 import { create } from 'zustand';
 
 interface StreakState {
-  streak: number;
+  streak: number | null;
   setStreak: (streak: number) => void;
 }
 
 export const useStreakStore = create<StreakState>((set) => ({
-  streak: 0,
+  streak: null,
   setStreak: (streak: number) => set({ streak: streak }),
 }));
