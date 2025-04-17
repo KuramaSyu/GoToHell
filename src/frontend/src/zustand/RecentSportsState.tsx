446e6cf8d1cba79f41279bf7a5d6b88350fefee5
kurama <paul.zenker.pz@gmail.com>
2025-04-17 17:37:43 +0200

    feat: added recentSports and setRecentSports

--- src/frontend/src/zustand/RecentSportsState.tsx
+++ src/frontend/src/zustand/RecentSportsState.tsx
@@ -1,12 +1,18 @@
 import { create } from 'zustand';
+import { SportsApiResponse } from '../models/Sport';
 
 interface RecentSportsState {
   refreshTrigger: number;
+  recentSports: SportsApiResponse | null;
   triggerRefresh: () => void;
+  setRecentSports: (recentSports: SportsApiResponse | null) => void;
 }
 
 export const useRecentSportsStore = create<RecentSportsState>((set) => ({
   refreshTrigger: 0,
+  recentSports: null,
   triggerRefresh: () =>
     set((state) => ({ refreshTrigger: state.refreshTrigger + 1 })),
+  setRecentSports: (recentSports: SportsApiResponse | null) =>
+    set({ recentSports: recentSports }),
 }));
