c9a804afc7be23073c08c5052bc77dbb9c0f1309
kurama <paul.zenker.pz@gmail.com>
2025-04-19 10:06:32 +0200

    refactor: mvoed SettingsSlider to own file

--- src/frontend/src/pages/Settings/Multiplier.tsx
+++ src/frontend/src/pages/Settings/Multiplier.tsx
@@ -6,6 +6,7 @@ import { getCookie, setCookie } from '../../utils/cookies';
 import { GameOverrideList, GameOverrideSettings } from './GameOverride';
 import { Multiplier, UserPreferences } from '../../models/Preferences';
 import { GenerateMarks } from '../../utils/Marks';
+import { SettingsSlider } from './SettingsSlider';
 
 export const MultiplierSettings: React.FC = () => {
   const { preferences, setPreferences } = usePreferenceStore();
@@ -55,74 +56,6 @@ export const MultiplierSettings: React.FC = () => {
         setSliderValue={setSliderValue}
         sliderValue={sliderValue}
       ></SettingsSlider>
-    </Box>
-  );
-};
-
-export interface SettingsSliderProperties {
-  min: number;
-  max: number;
-  sliderValue: number;
-  setSliderValue: React.Dispatch<React.SetStateAction<number>>;
-  saveValue: (game: string | null, value: number) => void;
-}
-export const SettingsSlider: React.FC<SettingsSliderProperties> = ({
-  min,
-  max,
-  sliderValue,
-  setSliderValue,
-  saveValue,
-}) => {
-  const { marks } = GenerateMarks(4, min, max);
-
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        gap: 5,
-        alignItems: 'center',
-        backdropFilter: 'blur(25px)',
-        padding: 5,
-        borderRadius: 5,
-        backgroundColor: 'rgba(0,0,0,0.2)',
-      }}
-    >
-      <OutlinedInput
-        value={sliderValue}
-        type="number"
-        onChange={(e) => {
-          const value = parseFloat(e.target.value) || min;
-          setSliderValue(value);
-          saveValue(null, value);
-        }}
-        inputProps={{
-          step: 0.05,
-          style: {
-            textAlign: 'center',
-          },
-        }}
-        sx={{
-          fontSize: '24px',
-          justifyContent: 'center',
-          alignItems: 'center',
-          display: 'flex',
-        }}
-      />
-      <Slider
-        size="medium"
-        value={sliderValue}
-        marks={marks}
-        onChange={(e, value) =>
-          setSliderValue(Array.isArray(value) ? value[0] ?? min : value)
-        }
-        onChangeCommitted={(e, value) =>
-          saveValue(null, Array.isArray(value) ? value[0] ?? min : value ?? min)
-        }
-        min={min}
-        max={max}
-        step={0.05}
-      />
     </Box>
   );
 };
---
+++ src/frontend/src/pages/Settings/SettingsSlider.tsx
@@ -0,0 +1,67 @@
+export interface SettingsSliderProperties {
+  min: number;
+  max: number;
+  sliderValue: number;
+  setSliderValue: React.Dispatch<React.SetStateAction<number>>;
+  saveValue: (game: string | null, value: number) => void;
+}
+export const SettingsSlider: React.FC<SettingsSliderProperties> = ({
+  min,
+  max,
+  sliderValue,
+  setSliderValue,
+  saveValue,
+}) => {
+  const { marks } = GenerateMarks(4, min, max);
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 5,
+        alignItems: 'center',
+        backdropFilter: 'blur(25px)',
+        padding: 5,
+        borderRadius: 5,
+        backgroundColor: 'rgba(0,0,0,0.2)',
+      }}
+    >
+      <OutlinedInput
+        value={sliderValue}
+        type="number"
+        onChange={(e) => {
+          const value = parseFloat(e.target.value) || min;
+          setSliderValue(value);
+          saveValue(null, value);
+        }}
+        inputProps={{
+          step: 0.05,
+          style: {
+            textAlign: 'center',
+          },
+        }}
+        sx={{
+          fontSize: '24px',
+          justifyContent: 'center',
+          alignItems: 'center',
+          display: 'flex',
+        }}
+      />
+      <Slider
+        size="medium"
+        value={sliderValue}
+        marks={marks}
+        onChange={(e, value) =>
+          setSliderValue(Array.isArray(value) ? value[0] ?? min : value)
+        }
+        onChangeCommitted={(e, value) =>
+          saveValue(null, Array.isArray(value) ? value[0] ?? min : value ?? min)
+        }
+        min={min}
+        max={max}
+        step={0.05}
+      />
+    </Box>
+  );
+};
