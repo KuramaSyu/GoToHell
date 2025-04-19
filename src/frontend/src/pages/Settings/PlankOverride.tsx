e596946ee34ce4174165cf6a58e0cc16bf3fb606
kurama <paul.zenker.pz@gmail.com>
2025-04-19 10:32:19 +0200

    Create PlankOverride.tsx

---
+++ src/frontend/src/pages/Settings/PlankOverride.tsx
@@ -0,0 +1,47 @@
+import { Box, OutlinedInput, Slider } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { SettingsSlider } from './SettingsSlider';
+import usePreferenceStore from '../../zustand/PreferenceStore';
+import { UserPreferences } from '../../models/Preferences';
+import { setCookie } from '../../utils/cookies';
+
+export const PlankOverride: React.FC = () => {
+  const { preferences, setPreferences } = usePreferenceStore();
+  const [maxSeconds, setMaxSeconds] = useState<number | null>(null);
+
+  // set plank when prefs change
+  useEffect(() => {
+    const plank = preferences.sport_specific.plank;
+    if (plank) {
+      setMaxSeconds(plank.seconds);
+    }
+  }, []);
+  const min = 60;
+  const max = 300;
+
+  const savePlankSeconds = (game: string | null, value: number) => {
+    setMaxSeconds(Number(value));
+    const newPreferences: UserPreferences = {
+      ...preferences,
+      sport_specific: {
+        ...preferences.sport_specific,
+        plank: {
+          seconds: value,
+        },
+      },
+    };
+    setPreferences(newPreferences);
+    setCookie('preferences', JSON.stringify(newPreferences), 999);
+  };
+
+  return (
+    <SettingsSlider
+      min={0}
+      max={360}
+      setSliderValue={setMaxSeconds}
+      sliderValue={maxSeconds}
+      saveValue={savePlankSeconds}
+      step={1}
+    ></SettingsSlider>
+  );
+};
