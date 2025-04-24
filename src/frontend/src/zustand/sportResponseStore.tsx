9d80570f89bb98868cb287b97059a72c68e0da60
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-04-24 18:16:43 +0200

    api: added func to return an empty response

--- src/frontend/src/zustand/sportResponseStore.tsx
+++ src/frontend/src/zustand/sportResponseStore.tsx
@@ -4,10 +4,14 @@ import { GetSportsResponse } from '../models/Sport';
 interface SportResponseState {
   sportResponse: GetSportsResponse | null;
   setSportResponse: (sportResponse: GetSportsResponse) => void;
+  emptySportsResponse: () => GetSportsResponse;
 }
 
 export const useSportResponseStore = create<SportResponseState>((set) => ({
   sportResponse: null,
   setSportResponse: (sportResponse: GetSportsResponse) =>
     set({ sportResponse: sportResponse }),
+  emptySportsResponse: () => {
+    return { games: {}, sports: {} };
+  },
 }));
