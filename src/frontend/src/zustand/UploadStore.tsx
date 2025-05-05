fcf65cb292738650d5fb46d9110215c3792f1efd
kurama <paul.zenker.pz@gmail.com>
2025-05-05 11:07:36 +0200

    feat: added setUpload

--- src/frontend/src/zustand/UploadStore.tsx
+++ src/frontend/src/zustand/UploadStore.tsx
@@ -4,12 +4,14 @@ import { create } from 'zustand';
 interface UploadState {
   uploadTrigger: number;
   triggerUpload: () => void;
+  setUpload: (n: number) => void;
 }
 
 const useUploadStore = create<UploadState>((set) => ({
   uploadTrigger: 0,
   triggerUpload: () =>
     set((state) => ({ uploadTrigger: state.uploadTrigger + 1 })),
+  setUpload: (n: number) => set({ uploadTrigger: n }),
 }));
 
 export default useUploadStore;
