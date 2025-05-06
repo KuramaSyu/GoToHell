04d6a96fcd07326366df29f8cd84330df90d0c8a
kurama <paul.zenker.pz@gmail.com>
2025-05-06 10:43:25 +0200

    refactor: FC for every Card

--- src/frontend/src/components/QuickActions/ModalOverviewCards.tsx
+++ src/frontend/src/components/QuickActions/ModalOverviewCards.tsx
@@ -3,7 +3,11 @@ import AbcIcon from '@mui/icons-material/Abc';
 import PinIcon from '@mui/icons-material/Pin';
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 import DialpadIcon from '@mui/icons-material/Dialpad';
-import { UploadCardButton } from './QuickActionEntries';
+import {
+  NumberCardButton,
+  SearchCardButton,
+  UploadCardButton,
+} from './QuickActionEntries';
 import { ICON_QICK_ACTION_SX, QuickActionEntry } from './Main';
 
 export const ModalOverview: React.FC = () => {
@@ -18,17 +22,9 @@ export const ModalOverview: React.FC = () => {
         py: 2,
       }}
     >
-      <QuickActionEntry
-        keys="Any Number"
-        title="Deaths"
-        icon={<DialpadIcon sx={ICON_QICK_ACTION_SX} />}
-      />
-      <QuickActionEntry
-        keys="Any Letter"
-        title="Sport or Game"
-        icon={<AbcIcon sx={ICON_QICK_ACTION_SX} />}
-      />
-      <UploadCardButton></UploadCardButton>
+      <NumberCardButton />
+      <SearchCardButton />
+      <UploadCardButton />
     </Box>
   );
 };
--- src/frontend/src/components/QuickActions/QuickActionEntries.tsx
+++ src/frontend/src/components/QuickActions/QuickActionEntries.tsx
@@ -1,6 +1,7 @@
 import PinIcon from '@mui/icons-material/Pin';
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 import DialpadIcon from '@mui/icons-material/Dialpad';
+import AbcIcon from '@mui/icons-material/Abc';
 import { useSportStore } from '../../useSportStore';
 import { ICON_QICK_ACTION_SX, QuickActionEntry } from './Main';
 
@@ -10,6 +11,26 @@ export const UploadCardButton: React.FC = () => {
       keys="Enter"
       title="Upload"
       icon={<KeyboardReturnIcon sx={ICON_QICK_ACTION_SX} />}
+    />
+  );
+};
+
+export const NumberCardButton: React.FC = () => {
+  return (
+    <QuickActionEntry
+      keys="Any Number"
+      title="Deaths"
+      icon={<DialpadIcon sx={ICON_QICK_ACTION_SX} />}
+    />
+  );
+};
+
+export const SearchCardButton: React.FC = () => {
+  return (
+    <QuickActionEntry
+      keys="Any Letter"
+      title="Sport or Game"
+      icon={<AbcIcon sx={ICON_QICK_ACTION_SX} />}
     />
   );
 };
