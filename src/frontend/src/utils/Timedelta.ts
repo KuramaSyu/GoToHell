22345f2482078c435b04d20507b1188ba2dc97df
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-05-11 16:54:04 +0200

    api: added biggestUnit() to Timedelta

--- src/frontend/src/utils/Timedelta.ts
+++ src/frontend/src/utils/Timedelta.ts
@@ -1,3 +1,9 @@
+export enum Unit {
+  seconds = 1,
+  minutes = 2,
+  hours = 3,
+}
+
 export class Timedelta {
   totalSeconds: number;
   constructor(seconds: number) {
@@ -12,5 +18,15 @@ export class Timedelta {
   }
   hours(): number {
     return Math.floor(this.totalSeconds / (60 * 60));
+  }
+
+  biggestUnit(): Unit {
+    if (this.hours() > 0) {
+      return Unit.hours;
+    } else if (this.minutes() > 0) {
+      return Unit.minutes;
+    } else {
+      return Unit.seconds;
+    }
   }
 }
