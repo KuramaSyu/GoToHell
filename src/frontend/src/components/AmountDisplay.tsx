49e9d63469e7689b7d860a818cb7eca3ff83d375
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-05-11 16:35:30 +0200

    refactor: improved use of different Display Types

--- src/frontend/src/components/AmountDisplay.tsx
+++ src/frontend/src/components/AmountDisplay.tsx
@@ -31,152 +31,106 @@ export const AMOUNT_DISPLAY_CONTENT_BOX_SX = {
   mt: { xs: -2, md: -4 }, // Remove weird padding from font
 };
 
+export function getAnimatedNumberSize(isMobile: boolean, numberKind: string) {
+  if (numberKind === 'time') {
+    return isMobile ? '3vh' : '6vh';
+  }
+  return isMobile ? BIG_NUMBER_SIZE_MOBILE : BIG_NUMBER_SIZE_DESKTOP;
+}
+
 export interface SportServiceProps {
   computedValue: number;
   isMobile: boolean;
 }
-export interface ISportService {
-  toExercisesString: () => string;
-  toReact: () => React.FC<SportServiceProps>;
-}
 
-class BaseSportService implements ISportService {
-  sport: string;
-  amount: number;
-  constructor(sport: string, amonut: number) {
-    this.sport = sport;
-    this.amount = amonut;
-  }
-  toExercisesString(): string {
-    return 'Exercises';
-  }
-  toReact(): React.FC<SportServiceProps> {
-    return ({ computedValue, isMobile }) => (
-      <PopNumber
-        value={computedValue}
-        font={NUMBER_FONT}
-        fontsize={isMobile ? BIG_NUMBER_SIZE_MOBILE : BIG_NUMBER_SIZE_DESKTOP}
-        stiffness={1000}
-        damping={300}
-        mass={1}
-      />
-    );
-  }
-}
+const NumberDisplay: React.FC<SportServiceProps> = ({
+  computedValue,
+  isMobile,
+}) => {
+  return (
+    <PopNumber
+      value={computedValue}
+      font={NUMBER_FONT}
+      fontsize={isMobile ? BIG_NUMBER_SIZE_MOBILE : BIG_NUMBER_SIZE_DESKTOP}
+      stiffness={1000}
+      damping={300}
+      mass={1}
+      key={'AnimatedNumber'}
+    />
+  );
+};
 
-class EmptySportService implements ISportService {
-  constructor() {
-    this.sport = '';
-  }
-  sport: string;
-
-  toExercisesString(): string {
-    return 'Exercises';
-  }
-  toReact(): React.FC<SportServiceProps> {
-    return ({ computedValue, isMobile }) => null;
-  }
-}
-
-class PlankService extends BaseSportService {
-  timedelta: Timedelta;
-  constructor(amount: number) {
-    super('plank', amount);
-    this.timedelta = new Timedelta(amount);
-  }
-
-  toExercisesString(): string {
-    return 'Seconds Plank';
-  }
-  toReact(): React.FC<SportServiceProps> {
-    return ({ computedValue, isMobile }) => {
-      const timedelta = new Timedelta(computedValue);
-      const seconds = timedelta.seconds();
-      const minutes = timedelta.minutes();
-      const hours = timedelta.hours();
-      return (
-        <Box>
-          {hours > 0 ? (
-            <Box sx={{ display: 'flex' }}>
-              <PopNumber
-                value={hours}
-                font={NUMBER_FONT}
-                fontsize={
-                  isMobile ? BIG_NUMBER_SIZE_MOBILE : BIG_NUMBER_SIZE_DESKTOP
-                }
-                stiffness={1000}
-                damping={300}
-                mass={1}
-              />
-              <Typography
-                fontFamily={NUMBER_FONT}
-                fontSize={BIG_NUMBER_SIZE_DESKTOP}
-              >
-                :
-              </Typography>
-            </Box>
-          ) : null}
-          {minutes > 0 ? (
-            <Box sx={{ display: 'flex' }}>
-              <PopNumber
-                value={minutes}
-                font={NUMBER_FONT}
-                fontsize={
-                  isMobile ? BIG_NUMBER_SIZE_MOBILE : BIG_NUMBER_SIZE_DESKTOP
-                }
-                stiffness={1000}
-                damping={300}
-                mass={1}
-              />
-              <Typography
-                fontFamily={NUMBER_FONT}
-                fontSize={BIG_NUMBER_SIZE_DESKTOP}
-              >
-                :
-              </Typography>
-            </Box>
-          ) : null}
-          {seconds > 0 ? (
-            <Box sx={{ display: 'flex' }}>
-              <PopNumber
-                value={seconds}
-                font={NUMBER_FONT}
-                fontsize={
-                  isMobile ? BIG_NUMBER_SIZE_MOBILE : BIG_NUMBER_SIZE_DESKTOP
-                }
-                stiffness={1000}
-                damping={300}
-                mass={1}
-              />
-              <Typography
-                fontFamily={NUMBER_FONT}
-                fontSize={BIG_NUMBER_SIZE_DESKTOP}
-              >
-                :
-              </Typography>
-            </Box>
-          ) : null}
+const TimeDisplay: React.FC<SportServiceProps> = ({
+  computedValue,
+  isMobile,
+}) => {
+  const timedelta = new Timedelta(computedValue);
+  const seconds = timedelta.seconds();
+  const minutes = timedelta.minutes();
+  const hours = timedelta.hours();
+  const numberKind = 'time';
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      {hours > 0 ? (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <PopNumber
+            value={hours}
+            font={NUMBER_FONT}
+            fontsize={getAnimatedNumberSize(isMobile, numberKind)}
+            stiffness={1000}
+            damping={300}
+            mass={1}
+          />
+          <Typography
+            fontFamily={NUMBER_FONT}
+            fontSize={getAnimatedNumberSize(isMobile, numberKind)}
+          >
+            :
+          </Typography>
         </Box>
-      );
-    };
-  }
-}
+      ) : null}
+      {minutes > 0 ? (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <PopNumber
+            value={minutes}
+            font={NUMBER_FONT}
+            fontsize={getAnimatedNumberSize(isMobile, numberKind)}
+            stiffness={1000}
+            damping={300}
+            mass={1}
+            zeroPadding={2}
+          />
+          <Typography
+            fontFamily={NUMBER_FONT}
+            fontSize={getAnimatedNumberSize(isMobile, numberKind)}
+          >
+            :
+          </Typography>
+        </Box>
+      ) : null}
+      {seconds > 0 ? (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <PopNumber
+            value={seconds}
+            font={NUMBER_FONT}
+            fontsize={getAnimatedNumberSize(isMobile, numberKind)}
+            stiffness={1000}
+            damping={300}
+            mass={1}
+            zeroPadding={2}
+          />
+        </Box>
+      ) : null}
+    </Box>
+  );
+};
 
-class SimpleFactory {
-  static createSportService(
-    sport: string | null,
-    amount: number
-  ): ISportService {
-    if (sport == null) {
-      return new EmptySportService();
-    }
-
-    switch (sport) {
-      case 'plank':
-        return new PlankService(amount);
-      default:
-        return new BaseSportService(sport, amount);
-    }
+export function getDisplayComponent(sport: string | undefined) {
+  switch (sport) {
+    case 'plank':
+      return TimeDisplay;
+    default:
+      return NumberDisplay;
   }
 }
 
@@ -190,17 +144,6 @@ export const AmountDisplay = () => {
   const isXL = useMediaQuery(theme.breakpoints.up('xl'));
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
-  const { sportService, SportNumberComponent } = useMemo(() => {
-    const sport_service = SimpleFactory.createSportService(
-      currentSport.sport,
-      amount
-    );
-    return {
-      sportService: sport_service,
-      SportNumberComponent: sport_service.toReact(),
-    };
-  }, [currentSport.sport]);
-
   if (currentSport.game == null || currentSport.sport == null) {
     return <Box></Box>;
   }
@@ -210,6 +153,8 @@ export const AmountDisplay = () => {
     currentSport.game!,
     amount
   );
+
+  const DisplayComponent = getDisplayComponent(currentSport.sport);
   // currentSport.game_multiplier! * amount * currentSport.sport_multiplier!;
 
   return (
@@ -247,10 +192,7 @@ export const AmountDisplay = () => {
           : null}
 
         <Box sx={{ mr: 2 }}>
-          <SportNumberComponent
-            computedValue={computedValue}
-            isMobile={isMobile}
-          />
+          <DisplayComponent computedValue={computedValue} isMobile={isMobile} />
         </Box>
         <Box sx={AMOUNT_DISPLAY_CONTENT_BOX_SX}>
           <Typography sx={AMOUNT_DISPLAY_TITLE_SX} fontFamily={'inherit'}>
