5e1586cbd727d36b1810171955f0776120c579af
kurama <paul.zenker.pz@gmail.com>
2025-05-11 15:45:54 +0200

    Create Timedelta.ts

---
+++ src/frontend/src/utils/Timedelta.ts
@@ -0,0 +1,16 @@
+export class Timedelta {
+  totalSeconds: number;
+  constructor(seconds: number) {
+    this.totalSeconds = seconds;
+  }
+
+  seconds(): number {
+    return this.totalSeconds % 60;
+  }
+  minutes(): number {
+    return Math.floor(this.totalSeconds / 60) % 60;
+  }
+  hours(): number {
+    return Math.floor(this.totalSeconds / (60 ^ 2));
+  }
+}
