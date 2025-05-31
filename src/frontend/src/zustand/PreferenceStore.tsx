29385ca6114ecef43cb46bdeb9a9d28780225b5e
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-05-31 14:04:15 +0200

    api: added preferencesLoaded flag

--- src/frontend/src/zustand/PreferenceStore.tsx
+++ src/frontend/src/zustand/PreferenceStore.tsx
@@ -4,11 +4,14 @@ import { defaultPreferences, UserPreferences } from '../models/Preferences';
 interface PreferencesState {
   preferences: UserPreferences;
   setPreferences: (preferences: UserPreferences) => void;
+  preferencesLoaded: boolean;
 }
 
 const usePreferenceStore = create<PreferencesState>((set) => ({
   preferences: defaultPreferences(),
-  setPreferences: (preferences) => set({ preferences: preferences }),
+  preferencesLoaded: false,
+  setPreferences: (preferences) =>
+    set({ preferences: preferences, preferencesLoaded: true }),
 }));
 
 export default usePreferenceStore;
