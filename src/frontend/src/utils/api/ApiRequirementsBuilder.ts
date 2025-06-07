905c3cbd08606df5e886894695cdf3a91fd28984
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-06-07 14:44:05 +0200

    api: added ApiRequirementABC

--- src/frontend/src/utils/api/ApiRequirementsBuilder.ts
+++ src/frontend/src/utils/api/ApiRequirementsBuilder.ts
@@ -3,11 +3,22 @@ import { useTotalScoreStore } from '../../zustand/TotalScoreStore';
 import { UserApi } from './Api';
 
 interface ApiReuqirement {
-  needsFetch: () => Boolean;
-  fetch: () => Promise<void>;
+  needsFetch(): Boolean;
+  fetch(): Promise<void>;
+  fetchIfNeeded(): Promise<void>;
 }
 
-export class UserRequirement implements ApiReuqirement {
+abstract class ApiRequirementABC implements ApiReuqirement {
+  abstract needsFetch(): Boolean;
+  abstract fetch(): Promise<void>;
+  async fetchIfNeeded(): Promise<void> {
+    if (this.needsFetch()) {
+      await this.fetch();
+    }
+  }
+}
+
+export class UserRequirement extends ApiRequirementABC {
   needsFetch(): Boolean {
     return useUserStore.getState().user === null;
   }
@@ -17,7 +28,7 @@ export class UserRequirement implements ApiReuqirement {
   }
 }
 
-export class TotalScoreRequirement implements ApiReuqirement {
+export class TotalScoreRequirement extends ApiRequirementABC {
   needsFetch(): Boolean {
     return useTotalScoreStore.getState().amounts.length === 0;
   }
