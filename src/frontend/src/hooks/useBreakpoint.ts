fb796af680622052185df662ccb6fca85195d4b0
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-06-13 15:41:54 +0200

    refactor: added utility hook for modile/tablet/desktop breakpoints

---
+++ src/frontend/src/hooks/useBreakpoint.ts
@@ -0,0 +1,9 @@
+import { useMediaQuery, useTheme } from '@mui/material';
+
+export const useBreakpoint = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
+  const isDesktop = useMediaQuery(theme.breakpoints.up('lg'));
+  const isXL = useMediaQuery(theme.breakpoints.up('xl'));
+};
