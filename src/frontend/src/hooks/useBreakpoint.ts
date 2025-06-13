7b472ade963bd411adc77690d4a40990c532cfba
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-06-13 15:43:52 +0200

    fix: added missing return for breakpoint utility hook

--- src/frontend/src/hooks/useBreakpoint.ts
+++ src/frontend/src/hooks/useBreakpoint.ts
@@ -6,4 +6,10 @@ export const useBreakpoint = () => {
   const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
   const isDesktop = useMediaQuery(theme.breakpoints.up('lg'));
   const isXL = useMediaQuery(theme.breakpoints.up('xl'));
+  return {
+    isMobile,
+    isTablet,
+    isDesktop,
+    isXL,
+  };
 };
