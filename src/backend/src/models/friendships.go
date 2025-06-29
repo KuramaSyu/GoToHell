d7e045956170fd39f4a011a88ab456c6e481c16b
kurama <paul.zenker.pz@gmail.com>
2025-06-29 13:26:44 +0200

    refactor: moved Friendships struct to /models

---
+++ src/backend/src/models/friendships.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+)
+
+type FriendshipStatus string
+
+const (
+	Pending  FriendshipStatus = "pending"
+	Accepted FriendshipStatus = "accepted"
+	blocked  FriendshipStatus = "blocked"
+)
+
+// SQL Table representing a friendship between person A <RequesterID> and person B <RecipientID>.
+// The status <Status> of the friendship is either pending, accepted or blocked
+type Friendships struct {
+	ID          Snowflake        `gorm:"primaryKey" json:"id"`
+	RequesterID Snowflake        `json:"requester_id"`
+	RecipientID Snowflake        `json:"recipient_id"`
+	Status      FriendshipStatus `json:"status"`
+	CreatedAt   time.Time        `json:"created_at"`
+}
