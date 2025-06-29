04a15c53bba5a8b4680c71e2a7ccc47f7ad14a98
kurama <paul.zenker.pz@gmail.com>
2025-06-29 13:27:00 +0200

    refactor: moved OverdueDeaths struct to /models

---
+++ src/backend/src/models/overdue_deaths.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	_ "github.com/mattn/go-sqlite3" // SQLite driver
+)
+
+// SQL Table representing the users (UserID) overdue deaths (Count) for a specific game (Game).
+// Game and UserID are unique, hence a composite primary key is used
+// instead of a serial primary key
+type OverdueDeaths struct {
+	UserID Snowflake `gorm:"primaryKey;autoIncrement:false" json:"user_id"`
+	Game   string    `gorm:"primaryKey;autoIncrement:false" json:"game"`
+	Count  int64     `json:"count"`
+}
