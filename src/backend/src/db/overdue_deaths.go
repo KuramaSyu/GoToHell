6352db5fb04638a6bd2149ac3a55c1c56443e8c8
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-02 17:01:01 +0200

    feat: implemented `OverdueDeathsRepository` into the gorm specific one

--- src/backend/src/db/overdue_deaths.go
+++ src/backend/src/db/overdue_deaths.go
@@ -1,4 +1,37 @@
 package db
 
+import (
+	. "github.com/KuramaSyu/GoToHell/src/backend/src/models"
+	"gorm.io/gorm"
+)
+
+// Specific implementation of `OverdueDeathRepository` for GORM
 type GormOverdueDeathsRepository struct {
+	db *gorm.DB
+}
+
+// automigrates the OverdueDeaths GORM table
+func (r *GormOverdueDeathsRepository) initRepo() {
+	r.db.AutoMigrate(&OverdueDeaths{})
+}
+
+// Inserts or updates a OverdueDeaths record in the DB.
+func (r *GormOverdueDeathsRepository) SetCount(userID Snowflake, game string, count int64) (*OverdueDeaths, error) {
+	var overdueDeaths = OverdueDeaths{
+		UserID: userID,
+		Game:   game,
+		Count:  count,
+	}
+	err := r.db.Save(overdueDeaths).Error
+	if err != nil {
+		return nil, err
+	}
+	return &overdueDeaths, err
+}
+
+// Returns a list with all OverdueDeaths records for the given user
+func (r *GormOverdueDeathsRepository) FetchAll(userID Snowflake) ([]OverdueDeaths, error) {
+	var overdueDeaths []OverdueDeaths
+	err := r.db.Where(&OverdueDeaths{UserID: userID}).Find(&overdueDeaths).Error
+	return overdueDeaths, err
 }
