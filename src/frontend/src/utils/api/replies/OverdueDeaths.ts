717862bee7491deae49a57bb4ee20fc132645c2e
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-04 20:18:13 +0200

    api: added interface for GET reply for /overdueDeaths

---
+++ src/frontend/src/utils/api/replies/OverdueDeaths.ts
@@ -0,0 +1,11 @@
+// interface for GET /overdueDeaths
+export interface GetOverdueDeathsReply {
+  data: OverdueDeaths[];
+}
+
+// one record of OverdueDeaths containing the user_id, the game and it's belonging count
+export interface OverdueDeaths {
+  user_id: string;
+  game: string;
+  count: number;
+}
