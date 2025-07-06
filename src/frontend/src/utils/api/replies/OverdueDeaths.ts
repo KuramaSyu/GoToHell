6b303598d7df4fbc9eac87de4e1f998d167020eb
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-06 20:50:44 +0200

    refactor: moved OverdueDeaths to models

--- src/frontend/src/utils/api/replies/OverdueDeaths.ts
+++ src/frontend/src/utils/api/replies/OverdueDeaths.ts
@@ -1,11 +1,6 @@
+import { OverdueDeaths } from '../../../models/OverdueDeaths';
+
 // interface for GET /overdue-deaths
 export interface GetOverdueDeathsReply {
   data: OverdueDeaths[];
-}
-
-// one record of OverdueDeaths containing the user_id, the game and it's belonging count
-export interface OverdueDeaths {
-  user_id: string;
-  game: string;
-  count: number;
 }
