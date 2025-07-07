786418afed7782b2acc99a33a0cef67c6d7f0c17
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-07 16:18:07 +0200

    docs: added docs with examples to `PostSportRequest`

--- src/backend/src/models/sport.go
+++ src/backend/src/models/sport.go
@@ -17,10 +17,20 @@ type Sport struct {
 
 // Row which is sent by the user. The rest will be added from
 // the API
-type PartialSport struct {
-	Kind     string    `json:"kind" binding:"required"`
-	Game     string    `json:"game" binding:"required"`
-	Amount   int       `json:"amount" binding:"required"`
-	Timedate time.Time `json:"timedate,omitempty"`
-	ID       Snowflake `json:"id,omitempty"`
+// swagger:model PostSportRequest
+type PostSportRequest struct {
+	// Kind of the sport
+	Kind string `json:"kind" binding:"required" example:"push-up"`
+
+	// The Game, this sport-record belongs to
+	Game string `json:"game" binding:"required" example:"overwatch"`
+
+	// The amount of Exercises done
+	Amount int `json:"amount" binding:"required" example:"42"`
+
+	// when the sport was done as UTC time - currently set by the API
+	Timedate time.Time `json:"timedate,omitempty" example:"1751897680.372402"`
+
+	// ID of the user, who did the sport - currently set by the API
+	ID Snowflake `json:"id,omitempty"`
 }
