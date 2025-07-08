aa1d3ea6359185cf38722de5f0986fd6e55175e7
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-08 19:53:05 +0200

    feat: added [delete] method for overdue deaths

--- src/backend/src/controllers/overdue_deaths.go
+++ src/backend/src/controllers/overdue_deaths.go
@@ -25,7 +25,14 @@ type PostOverdueDeathsRequest struct {
 // PostOverdueDeathsReply is the reply sent when doing [post] /overdue_deaths
 // swagger:model PostOverdueDeathsReply
 type PostOverdueDeathsReply struct {
+	// Data structure containing a user's overdue deaths
 	Data OverdueDeaths `json:"data"`
+}
+
+// DeleteOverdueDeathsRequest is the request to delete a user's overdue deaths record for a specific game.
+// swagger:model DeleteOverdueDeathsRequest
+type DeleteOverdueDeathsRequest struct {
+	Game string `json:"game" binding:"required" example:"overwatch"`
 }
 
 // FriendsController manages friendship endpoints.
@@ -92,6 +99,37 @@ func (oc *OverdueDeathsController) Post(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, PostOverdueDeathsReply{Data: *data})
+}
+
+// @Summary Deletes a user's overdue deaths record for a specific game
+// @Tags OverdueDeaths
+// @Accept json
+// @Produce json
+// @Security CookieAuth
+// @Param request body DeleteOverdueDeathsRequest true "Payload containing the game to delete the overdue deaths for"
+// @Success 200 {object} DeleteOverdueDeathsRequest
+// @Failure 400 {object} ErrorReply
+// @Failure 500 {object} ErrorReply
+// @Router /api/overdue_deaths [delete]
+func (oc *OverdueDeathsController) Delete(c *gin.Context) {
+	user, status, err := UserFromSession(c)
+	if err != nil {
+		SetGinError(c, status, err)
+		return
+	}
+
+	var req DeleteOverdueDeathsRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		SetGinError(c, http.StatusBadRequest, err)
+		return
+	}
+
+	err = oc.repo.Delete(user.ID, req.Game)
+	if err != nil {
+		SetGinError(c, http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, req)
 }
 
 func SetGinError(c *gin.Context, status int, err error) {
