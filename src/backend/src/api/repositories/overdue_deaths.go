e876acb46ed41be23ff7dc5ed3d8e7f107a9a35b
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-08 20:21:10 +0200

    api: added CreateCount and UpdateCount

--- src/backend/src/api/repositories/overdue_deaths.go
+++ src/backend/src/api/repositories/overdue_deaths.go
@@ -6,6 +6,8 @@ import . "github.com/KuramaSyu/GoToHell/src/backend/src/models"
 type OverdueDeathRepository interface {
 	InitRepo() error
 	SetCount(userID Snowflake, game string, count int64) (*OverdueDeaths, error)
+	CreateCount(userID Snowflake, game string, count int64) (*OverdueDeaths, error)
+	UpdateCount(userID Snowflake, game string, count int64) (*OverdueDeaths, error)
 	FetchAll(useID Snowflake) ([]OverdueDeaths, error)
 	Delete(userID Snowflake, game string) error
 }
