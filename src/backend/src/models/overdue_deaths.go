4ca4022f2426de131d2723b7cb7189e9d00d8e54
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-08 19:54:08 +0200

    docs: added examples to OverdueDeaths

--- src/backend/src/models/overdue_deaths.go
+++ src/backend/src/models/overdue_deaths.go
@@ -7,8 +7,9 @@ import (
 // SQL Table representing the users (UserID) overdue deaths (Count) for a specific game (Game).
 // Game and UserID are unique, hence a composite primary key is used
 // instead of a serial primary key
+// swagger:model OverdueDeaths
 type OverdueDeaths struct {
-	UserID Snowflake `gorm:"primaryKey;autoIncrement:false" json:"user_id"`
-	Game   string    `gorm:"primaryKey;autoIncrement:false" json:"game"`
-	Count  int64     `json:"count"`
+	UserID Snowflake `gorm:"primaryKey;autoIncrement:false" json:"user_id" example:"348922315062044675"`
+	Game   string    `gorm:"primaryKey;autoIncrement:false" json:"game" example:"overwatch"`
+	Count  int64     `json:"count" example:"69"`
 }
