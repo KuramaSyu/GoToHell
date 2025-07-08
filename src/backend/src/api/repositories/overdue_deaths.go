8f07b7e3af190b8df0da60d4738180f1988c3fc1
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-08 19:52:46 +0200

    api: added Delete() to OverdueDeathsRepository

--- src/backend/src/api/repositories/overdue_deaths.go
+++ src/backend/src/api/repositories/overdue_deaths.go
@@ -7,4 +7,5 @@ type OverdueDeathRepository interface {
 	InitRepo() error
 	SetCount(userID Snowflake, game string, count int64) (*OverdueDeaths, error)
 	FetchAll(useID Snowflake) ([]OverdueDeaths, error)
+	Delete(userID Snowflake, game string) error
 }
