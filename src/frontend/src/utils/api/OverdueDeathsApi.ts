a06cf23f284e63ebced935585f3c6bcfb7364083
kurama <paul.zenker.pz@gmail.com>
2025-07-11 15:47:27 +0200

    feat: added OverdueDeathsApi with get() and post()

---
+++ src/frontend/src/utils/api/OverdueDeathsApi.ts
@@ -0,0 +1,131 @@
+import { OverdueDeaths } from '../../models/OverdueDeaths';
+import { BACKEND_BASE } from '../../statics';
+import { useUserStore } from '../../userStore';
+import { useOverdueDeathsStore } from '../../zustand/OverdueDeathsStore';
+import {
+  GetOverdueDeathsReply,
+  PostOverdueDeathsReply,
+} from './replies/OverdueDeaths';
+import { PostOverdueDeathsRequest } from './requests/OverdueDeaths';
+
+export abstract class BasicApi {
+  // represents the backend methods, which are needed for user purposes
+  logError(url_part: string, error: any): void {
+    console.error(
+      `Error fetching ${BACKEND_BASE}${url_part}:`,
+      JSON.stringify(error)
+    );
+  }
+}
+export class OverdueDeathsApi extends BasicApi {
+  /**
+   * posts an record of OverdueDeaths for logged in user
+   * to /api/overdue-deaths POST
+   *
+   * @Note
+   * updates the useOverdueDeathsStore Zustand for the posted game
+   *
+   * @throws Error: if the fetch fails
+   *
+   * @returns
+   * PostOverdueDeathsReply | null: the Response or null if failed
+   */
+  async post(
+    game: string,
+    count: number
+  ): Promise<PostOverdueDeathsReply | null> {
+    const API_ENDPOINT = '/api/overdue-deaths';
+    // get user
+    const user = useUserStore.getState().user;
+    if (user === null) {
+      return null;
+    }
+    const url = new URL(`${BACKEND_BASE}${API_ENDPOINT}`);
+
+    try {
+      const response = await fetch(url, {
+        credentials: 'include',
+        method: 'POST',
+        body: JSON.stringify({
+          game: game,
+          count: count,
+        } as PostOverdueDeathsRequest),
+      });
+
+      const result = await response.json();
+      if (response.ok) {
+        // get zustand setter
+        const setOverdueDeaths =
+          useOverdueDeathsStore.getState().setOverdueDeaths;
+
+        // cast result
+        var reply = result as PostOverdueDeathsReply;
+
+        // update zustand for the new game
+        const prev = useOverdueDeathsStore.getState().overdueDeathsList;
+        setOverdueDeaths([
+          ...prev.filter((p) => p.game !== reply.data.game),
+          reply.data,
+        ]);
+
+        return reply;
+      } else {
+        this.logError(API_ENDPOINT, result);
+      }
+      return null;
+    } catch (error) {
+      this.logError(API_ENDPOINT, error);
+      throw error;
+    }
+  }
+
+  /**
+   * fetches OverdueDeaths for logged in user
+   * from /api/overdue-deaths GET
+   *
+   * @Note
+   * sets the useOverdueDeathsStore Zustand
+   *
+   * @throws Error: if the fetch fails
+   *
+   * @returns
+   * GetOverdueDeathsReply | null: the Response or null if failed
+   */
+  async get(): Promise<GetOverdueDeathsReply | null> {
+    const API_ENDPOINT = '/api/overdue-deaths';
+    // get user
+    const user = useUserStore.getState().user;
+    if (user === null) {
+      return null;
+    }
+    const url = new URL(`${BACKEND_BASE}${API_ENDPOINT}`);
+
+    try {
+      const response = await fetch(url, {
+        credentials: 'include',
+        method: 'GET',
+      });
+
+      const result = await response.json();
+      if (response.ok) {
+        // get zustand setter
+        const setOverdueDeaths =
+          useOverdueDeathsStore.getState().setOverdueDeaths;
+
+        // cast result
+        var reply = result as GetOverdueDeathsReply;
+
+        // set zustand
+        setOverdueDeaths(reply.data);
+
+        return reply;
+      } else {
+        this.logError(API_ENDPOINT, result);
+      }
+      return null;
+    } catch (error) {
+      this.logError(API_ENDPOINT, error);
+      throw error;
+    }
+  }
+}
