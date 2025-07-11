103464e28a999dd4a8f79461b6a97932dcdd5f8d
kurama <paul.zenker.pz@gmail.com>
2025-07-11 15:47:41 +0200

    api: added interface PostOverdueDeathsReply

--- src/frontend/src/utils/api/replies/OverdueDeaths.ts
+++ src/frontend/src/utils/api/replies/OverdueDeaths.ts
@@ -4,3 +4,10 @@ import { OverdueDeaths } from '../../../models/OverdueDeaths';
 export interface GetOverdueDeathsReply {
   data: OverdueDeaths[];
 }
+
+// interface for POST /overdue-deaths
+// this is just the exact data stored to the backend.
+// most likely the same as the Post Request
+export interface PostOverdueDeathsReply {
+  data: OverdueDeaths;
+}
