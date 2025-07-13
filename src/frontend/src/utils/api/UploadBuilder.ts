db3c8e957e9637b08b6f09b69547caeb3892ed7d
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-13 15:25:13 +0200

    refactor: use uploadStrategy in upload function

--- src/frontend/src/utils/api/UploadBuilder.ts
+++ src/frontend/src/utils/api/UploadBuilder.ts
@@ -23,6 +23,21 @@ abstract class UploadStrategyABC {
 }
 
 class PostSportUploadStrategy extends UploadStrategyABC {
+  /**
+   * updates the zustand stores:
+   * - `TotalScoreStore` to refresh the total scores `results`
+   * - `DeathAmountStore` to reset the death amount to 0
+   */
+  updateStores(results: SportScore[] | null): void {
+    // TODO: this also triggers timeline to update
+    // recent activities. make better with websocket
+    if (results !== null) {
+      useTotalScoreStore.getState().setAmounts(results);
+      useTotalScoreStore.getState().triggerRefresh(); // update total scores
+    }
+    useDeathAmountStore.getState().setAmount(0);
+  }
+
   async upload(): Promise<null> {
     const wrapped = this.uploadBuilder;
     if (!wrapped.user) {
@@ -65,7 +80,7 @@ class PostSportUploadStrategy extends UploadStrategyABC {
           console.log(data.results);
 
           if (wrapped.triggerRefresh) {
-            wrapped.updateStores(parsed_data.results); // update total scores and death amount
+            this.updateStores(parsed_data.results); // update total scores and death amount
           }
         }
         return null;
@@ -254,73 +269,17 @@ export class UploadBuilder {
     this.error = new UploadError(message);
   }
 
-  async upload(): Promise<{ message?: string; results?: SportScore[] }> {
-    if (!this.user) {
-      throw new UploadError('Please Login with Discord');
-    }
-    if (this.deathAmount === 0) {
-      throw new UploadError('Yeah, just upload nothing. Good idea indeed');
-    }
-
-    if (this.sport === null || this.sport.sport === null) {
-      throw new UploadError(
-        `What? Should I upload ${this.deathAmount} apples? Perhaps oranges?`
-      );
-    }
-    const startTime = new Date().getTime();
-    try {
-      const sport = new SportRow(
-        this.sport.sport!,
-        this.sport.game!,
-        this.exerciseAmount!
-      );
-      console.timeLog(`Upload Sport: ${sport.toJson()}`);
-      var data = await new UserApi().postSports([sport]);
-
-      // wait artificially 1s, for upload animation
-      // Calculate elapsed time in milliseconds.
-      const elapsedTime = new Date().getTime() - startTime;
-      const minimumDuration = 1000;
-
-      // Wait for the rest of the minimum duration if necessary.
-      if (elapsedTime < minimumDuration) {
-        const remainingTime = minimumDuration - elapsedTime;
-        await new Promise((resolve) => setTimeout(resolve, remainingTime));
-      }
-      if (data !== null) {
-        const parsed_data = data;
-
-        if (parsed_data.results) {
-          // data.results is now an array of SportAmount
-          console.log(data.results);
-
-          if (this.triggerRefresh) {
-            this.updateStores(parsed_data.results); // update total scores and death amount
-          }
-        }
-        return parsed_data;
-      } else {
-        throw new UploadError(
-          'Response was null, perhaps you are offline? This should not happen.'
-        );
-      }
-    } catch (error) {
-      throw new UploadError(String(error));
-    }
-  }
-
   /**
-   * updates the zustand stores:
-   * - `TotalScoreStore` to refresh the total scores `results`
-   * - `DeathAmountStore` to reset the death amount to 0
+   * uploads the data to the server using the UploadStrategy and returns a promise.
+   * @throws UploadError if the upload fails
    */
-  updateStores(results: SportScore[] | null): void {
-    // TODO: this also triggers timeline to update
-    // recent activities. make better with websocket
-    if (results !== null) {
-      useTotalScoreStore.getState().setAmounts(results);
-      useTotalScoreStore.getState().triggerRefresh(); // update total scores
+  async upload(): Promise<null> {
+    try {
+      const _ = await this.uploadStrategy.upload();
+    } catch (error) {
+      this.setErrorMessage(String(error));
+      throw this.error;
     }
-    useDeathAmountStore.getState().setAmount(0);
+    return null;
   }
 }
