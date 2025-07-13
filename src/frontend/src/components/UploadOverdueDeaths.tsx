ad651a68dea4fcbd42070887608b545239a02b2d
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-13 15:09:28 +0200

    Create UploadOverdueDeaths.tsx

---
+++ src/frontend/src/components/UploadOverdueDeaths.tsx
@@ -0,0 +1,137 @@
+import { useEffect, useState } from 'react';
+import {
+  Box,
+  Typography,
+  Snackbar,
+  CircularProgress,
+  useMediaQuery,
+} from '@mui/material';
+import { useSportStore } from '../useSportStore';
+import { useDeathAmountStore } from './NumberSlider';
+import SendIcon from '@mui/icons-material/Send';
+import { useUserStore } from '../userStore';
+import SportRow, { SportScore } from '../models/Sport';
+import useErrorStore from '../zustand/Error';
+import { alpha } from '@mui/material/styles';
+import { useTotalScoreStore } from '../zustand/TotalScoreStore';
+import AnimatedButton from './AnimatedButton';
+import { useRecentSportsStore } from '../zustand/RecentSportsState';
+import useCalculatorStore from '../zustand/CalculatorStore';
+import { useThemeStore } from '../zustand/useThemeStore';
+import { UserApi } from '../utils/api/Api';
+import useUploadStore from '../zustand/UploadStore';
+import { useBreakpoint } from '../hooks/useBreakpoint';
+import { UploadBuilder } from '../utils/api/UploadBuilder';
+import { UploadError } from '../utils/errors/UploadError';
+import SnoozeIcon from '@mui/icons-material/Snooze';
+
+type SnackbarState = 'uploading' | 'uploaded' | 'failed' | null;
+
+export const UploadOverdueDeaths = () => {
+  const { currentSport } = useSportStore();
+  const { amount, setAmount: setDeathAmount } = useDeathAmountStore();
+  const { user } = useUserStore();
+  const { setErrorMessage } = useErrorStore();
+  const [snackbarState, setSnackbarState] = useState<SnackbarState>(null);
+  const { setAmounts, triggerRefresh: triggerScoreRefresh } =
+    useTotalScoreStore();
+  const { calculator } = useCalculatorStore();
+  const { uploadTrigger } = useUploadStore();
+  const { isMobile } = useBreakpoint();
+
+  // for triggers coming from outside (eg shortcut modal)
+
+  const OnUploadClick = async () => {
+    try {
+      const startTime = new Date().getTime();
+      const uploadBuilder = UploadBuilder.default().setStoreUpdate(false);
+
+      setSnackbarState('uploading');
+
+      // set timer to wait arteficially 1s, for upload animation
+      const minimumDuration = 1000;
+      const parsed_data = await uploadBuilder.upload();
+      const elapsedTime = new Date().getTime() - startTime;
+
+      // Wait for the rest of the minimum duration if necessary.
+      if (elapsedTime < minimumDuration) {
+        const remainingTime = minimumDuration - elapsedTime;
+        await new Promise((resolve) => setTimeout(resolve, remainingTime));
+      }
+      setSnackbarState('uploaded');
+
+      if (parsed_data.results) {
+        // data.results is now an array of SportAmount
+        uploadBuilder.updateStores(parsed_data.results);
+      }
+    } catch (e) {
+      // handle uplaod error - description is in error included
+      setSnackbarState('failed');
+      setErrorMessage(String(e));
+    }
+
+    setTimeout(() => setSnackbarState(null), 2000);
+  };
+  if (
+    !(
+      currentSport &&
+      user &&
+      currentSport.sport_multiplier &&
+      currentSport.game_multiplier
+    )
+  ) {
+    return null;
+  }
+  const computedValue = calculator.calculate_amount(
+    currentSport.sport!,
+    currentSport.game!,
+    amount
+  );
+  const DURATION = amount !== 0 ? Math.max(40 - amount ** 1.5, 8) : 0;
+
+  return (
+    <Box>
+      <AnimatedButton onClick={OnUploadClick} duration={DURATION}>
+        <Box
+          sx={{
+            px: 1,
+            py: 1,
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: 2,
+          }}
+        >
+          <SnoozeIcon></SnoozeIcon>
+        </Box>
+      </AnimatedButton>
+      <Snackbar
+        open={snackbarState != null}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        slotProps={{
+          content: {
+            sx: {
+              backgroundColor: (theme: any) =>
+                alpha(theme.palette.secondary.main, 0.6),
+              color: (theme: any) => theme.palette.primary.contrastText,
+            },
+          },
+        }}
+        message={
+          snackbarState === 'uploading' ? (
+            <Box display="flex" alignItems="center">
+              <CircularProgress size={30} sx={{ mr: 1 }} />
+              Uploading...
+            </Box>
+          ) : snackbarState === 'uploaded' ? (
+            'Uploaded!'
+          ) : snackbarState === null ? (
+            ''
+          ) : (
+            'Failed!'
+          )
+        }
+      />
+    </Box>
+  );
+};
