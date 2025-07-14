3b5065b1853f0f3dee8d895470362c6dd3ec961c
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-14 21:27:06 +0200

    ui: added AnimatedRoundBtn

--- src/frontend/src/components/AnimatedButton.tsx
+++ src/frontend/src/components/AnimatedButton.tsx
@@ -7,12 +7,14 @@ interface AnimatedButtonProps {
   children: ReactNode;
   onClick?: () => void;
   duration?: number; // new prop added to control animation duration
+  circular?: boolean;
 }
 
 const AnimatedButton = ({
   children,
   onClick,
   duration = 10,
+  circular = false,
 }: AnimatedButtonProps) => {
   const theme = useTheme();
 
@@ -26,7 +28,17 @@ const AnimatedButton = ({
     color: theme.palette.common.white,
     fontWeight: 'bold',
     background: isAnimationActive
-      ? `linear-gradient(90deg, 
+      ? circular
+        ? `conic-gradient(from 0deg, 
+            ${theme.palette.secondary.light}, 
+            ${theme.palette.secondary.main}, 
+            ${theme.palette.secondary.dark}, 
+            ${theme.palette.primary.light}, 
+            ${theme.palette.primary.main}, 
+            ${theme.palette.primary.dark}, 
+            ${theme.palette.secondary.light}
+            )`
+        : `linear-gradient(90deg, 
           ${theme.palette.secondary.light}, 
           ${theme.palette.secondary.main}, 
           ${theme.palette.secondary.dark}, 
@@ -58,4 +70,52 @@ const AnimatedButton = ({
   );
 };
 
+export const AnimatedRoundBtn = ({
+  children,
+  onClick,
+  duration = 10,
+}: AnimatedButtonProps) => {
+  const theme = useTheme();
+
+  const isAnimationActive = duration !== 0;
+
+  const RoundBtn = styled(motion.create(Button))({
+    position: 'relative',
+    overflow: 'hidden',
+    width: '100%',
+    height: '100%',
+    borderRadius: '50%',
+    borderWidth: '3px',
+    color: theme.palette.common.white,
+    fontWeight: 'bold',
+    background: isAnimationActive
+      ? `conic-gradient(from 0deg, 
+          ${theme.palette.secondary.light}, 
+          ${theme.palette.secondary.main}, 
+          ${theme.palette.secondary.dark}, 
+          ${theme.palette.primary.light}, 
+          ${theme.palette.primary.main}, 
+          ${theme.palette.primary.dark}, 
+          ${theme.palette.secondary.light}
+          )`
+      : 'transparent',
+  });
+
+  return (
+    <RoundBtn
+      {...(isAnimationActive
+        ? {
+            animate: {
+              backgroundPosition: ['0% 50%', '100% 50%', '200% 50%', '0% 50%'],
+            },
+            transition: { duration, repeat: Infinity, ease: 'linear' },
+          }
+        : {})}
+      variant={isAnimationActive ? 'contained' : 'outlined'}
+      onClick={onClick}
+    >
+      {children}
+    </RoundBtn>
+  );
+};
 export default AnimatedButton;
