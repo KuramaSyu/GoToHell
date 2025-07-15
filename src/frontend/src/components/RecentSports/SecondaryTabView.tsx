1d3b77bffdba5dcc1d7d2ca47fdd51c9a32245ea
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-15 18:32:05 +0200

    ux: added Fade in and out to secondary tab

--- src/frontend/src/components/RecentSports/SecondaryTabView.tsx
+++ src/frontend/src/components/RecentSports/SecondaryTabView.tsx
@@ -1,4 +1,4 @@
-import { alpha, Box, Tab, Tabs } from '@mui/material';
+import { alpha, Box, Fade, Tab, Tabs } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useBreakpoint } from '../../hooks/useBreakpoint';
 import useCalculatorStore from '../../zustand/CalculatorStore';
@@ -37,92 +37,87 @@ export const SecondaryTabView: React.FC = () => {
   if (!isXL) amountOfTabs += 1;
   if (currentOverdueDeaths) amountOfTabs += 1;
 
-  if (amountOfTabs === 0) {
-    return null;
-  }
-
-  if (amountOfTabs === 1) {
-  }
-
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '100%',
-        flexShrink: 1,
-        flexGrow: 0,
-        alignItems: 'center',
-      }}
-    >
-      <Tabs
-        onChange={handleChange}
-        value={activeTab}
-        textColor="primary"
-        indicatorColor="primary"
-        centered
-        sx={{
-          backgroundColor: alpha('#000000', 0.2),
-          borderRadius: '50px',
-          padding: '5px',
-          backdropFilter: 'blur(10px)',
-          p: 1,
-          px: 3,
-          width: 'fit-content',
-        }}
-      >
-        <Tab
-          label="Overdue Deaths"
-          sx={{ minWidth: isXL ? 150 : 100, width: 'auto' }}
-        />
-
-        {!isXL ? (
-          <Tab
-            label="Calculation"
-            sx={{ minWidth: isXL ? 150 : 100, width: 'auto' }}
-          />
-        ) : null}
-      </Tabs>
+    <Fade in={amountOfTabs > 0} timeout={1000}>
       <Box
         sx={{
-          mt: 2,
           display: 'flex',
-          position: 'relative',
+          flexDirection: 'column',
           width: '100%',
-          justifyContent: 'center',
-          minHeight: 129,
-          overflow: 'hidden',
+          flexShrink: 1,
+          flexGrow: 0,
+          alignItems: 'center',
         }}
       >
-        {activeTab == 0 && (
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexGrow: '1',
-            }}
-          >
-            <OverdueDeaths />
-          </Box>
-        )}
-        {activeTab == 1 && (
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'end',
-              alignItems: 'end',
-              flexGrow: '1',
-            }}
-          >
-            {calculator.make_box(
-              currentSport.sport!,
-              currentSport.game!,
-              amount
-            )}
-          </Box>
-        )}
+        <Tabs
+          onChange={handleChange}
+          value={activeTab}
+          textColor="primary"
+          indicatorColor="primary"
+          centered
+          sx={{
+            backgroundColor: alpha('#000000', 0.2),
+            borderRadius: '50px',
+            padding: '5px',
+            backdropFilter: 'blur(10px)',
+            p: 1,
+            px: 3,
+            width: 'fit-content',
+          }}
+        >
+          <Tab
+            label="Overdue Deaths"
+            sx={{ minWidth: isXL ? 150 : 100, width: 'auto' }}
+          />
+
+          {!isXL ? (
+            <Tab
+              label="Calculation"
+              sx={{ minWidth: isXL ? 150 : 100, width: 'auto' }}
+            />
+          ) : null}
+        </Tabs>
+        <Box
+          sx={{
+            mt: 2,
+            display: 'flex',
+            position: 'relative',
+            width: '100%',
+            justifyContent: 'center',
+            minHeight: 129,
+            overflow: 'hidden',
+          }}
+        >
+          {activeTab == 0 && (
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexGrow: '1',
+              }}
+            >
+              <OverdueDeaths />
+            </Box>
+          )}
+          {activeTab == 1 && (
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'end',
+                alignItems: 'end',
+                flexGrow: '1',
+              }}
+            >
+              {calculator.make_box(
+                currentSport.sport!,
+                currentSport.game!,
+                amount
+              )}
+            </Box>
+          )}
+        </Box>
       </Box>
-    </Box>
+    </Fade>
   );
 };
