5398e5aef9a58bd9684c6b1da20eddc3425e6e3e
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-15 17:39:17 +0200

    chore: fully removed snackbar from sleep button

--- src/frontend/src/components/UploadOverdueDeaths.tsx
+++ src/frontend/src/components/UploadOverdueDeaths.tsx
@@ -26,6 +26,7 @@ import { UploadBuilder } from '../utils/api/UploadBuilder';
 import { UploadError } from '../utils/errors/UploadError';
 import SnoozeIcon from '@mui/icons-material/Snooze';
 import { clamp } from 'date-fns';
+import { blendWithContrast } from '../utils/blendWithContrast';
 
 type SnackbarState = 'uploading' | 'uploaded' | 'failed' | null;
 
@@ -34,37 +35,23 @@ export const UploadOverdueDeaths = () => {
   const { amount, setAmount: setDeathAmount } = useDeathAmountStore();
   const { user } = useUserStore();
   const { setErrorMessage } = useErrorStore();
-  const [snackbarState, setSnackbarState] = useState<SnackbarState>(null);
-  const { setAmounts, triggerRefresh: triggerScoreRefresh } =
-    useTotalScoreStore();
+  useTotalScoreStore();
   const { calculator } = useCalculatorStore();
-  const { uploadTrigger } = useUploadStore();
-  const { isMobile } = useBreakpoint();
   const { theme } = useThemeStore();
 
   // for triggers coming from outside (eg shortcut modal)
 
   const OnUploadClick = async () => {
     try {
-      const startTime = new Date().getTime();
       const uploadBuilder = UploadBuilder.default()
         .setUploadStrategy('overdueDeaths')
         .setStoreUpdate(false);
 
-      //setSnackbarState('uploading');
-
-      // set timer to wait arteficially 1s, for upload animation
       await uploadBuilder.upload();
       uploadBuilder.updateStores();
-
-      //setSnackbarState('uploaded');
     } catch (e) {
-      // handle uplaod error - description is in error included
-      //setSnackbarState('failed');
       setErrorMessage(String(e));
     }
-
-    setTimeout(() => setSnackbarState(null), 2000);
   };
   if (
     !(
@@ -76,12 +63,8 @@ export const UploadOverdueDeaths = () => {
   ) {
     return null;
   }
-  const computedValue = calculator.calculate_amount(
-    currentSport.sport!,
-    currentSport.game!,
-    amount
-  );
-  const duration = amount !== 0 ? Math.max(40 - amount ** 1.5, 8) : 0;
+
+  const duration = amount !== 0 ? Math.max(20 - amount ** 1.5, 1) : 0;
   const isAnimationActive = duration !== 0;
   return (
     <Box>
@@ -99,41 +82,14 @@ export const UploadOverdueDeaths = () => {
             sx={{
               fontSize: 'clamp(2rem, 4vh, 6rem)',
               color: isAnimationActive
-                ? darken(theme.palette.primary.main, 0.1)
-                : darken(theme.palette.primary.main, 0.3),
+                ? blendWithContrast(theme.palette.primary.main, theme, 0.3)
+                : darken(theme.palette.primary.main, 0.2),
             }}
           >
             <SnoozeIcon fontSize="inherit"></SnoozeIcon>
           </Box>
         </AnimatedRoundBtn>
       </Box>
-      {/* <Snackbar
-        open={snackbarState != null}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-        slotProps={{
-          content: {
-            sx: {
-              backgroundColor: (theme: any) =>
-                alpha(theme.palette.secondary.main, 0.6),
-              color: (theme: any) => theme.palette.primary.contrastText,
-            },
-          },
-        }}
-        message={
-          snackbarState === 'uploading' ? (
-            <Box display="flex" alignItems="center">
-              <CircularProgress size={30} sx={{ mr: 1 }} />
-              Uploading...
-            </Box>
-          ) : snackbarState === 'uploaded' ? (
-            'Uploaded!'
-          ) : snackbarState === null ? (
-            ''
-          ) : (
-            'Failed!'
-          )
-        }
-      /> */}
     </Box>
   );
 };
