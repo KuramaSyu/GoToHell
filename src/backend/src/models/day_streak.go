6c480639f9be001076f4d7b1ff60c80273046270
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-07-17 16:21:37 +0200

    docs: added examples to DayStreak

--- src/backend/src/models/day_streak.go
+++ src/backend/src/models/day_streak.go
@@ -1,6 +1,6 @@
 package models
 
 type DayStreak struct {
-	UserID Snowflake `json:"user_id"`
-	Days   int       `json:"days"`
+	UserID Snowflake `json:"user_id" example:"123456789012345678"`
+	Days   int       `json:"days" example:"54"`
 }
