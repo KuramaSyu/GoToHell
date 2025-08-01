766e4fe2bb7b59e35019749b7decc76c068a328d
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-08-01 14:37:11 +0200

    feat: added bare SportSelectionDialog

---
+++ src/frontend/src/pages/MainPage/Dialogs/SportSelectionDialog.tsx
@@ -0,0 +1,27 @@
+import { useMemo } from 'react';
+import { useSportResponseStore } from '../../../zustand/sportResponseStore';
+import { DialogStateProps, SelectionDialog } from './SelectionDialog';
+import { SportEntry } from '../QuickActions/SearchModal';
+
+export interface SportSelectionDialogProps {
+  state: DialogStateProps;
+}
+
+export const SportSelectionDialog: React.FC<SportSelectionDialogProps> = ({
+  state: { open, setOpen },
+}) => {
+  const { sportResponse } = useSportResponseStore();
+  const sports: SportEntry[] = useMemo(() => {
+    if (!sportResponse || !sportResponse.sports) return [];
+    return Object.keys(sportResponse.sports).map((s) => new SportEntry(s));
+  }, [sportResponse]);
+
+  return (
+    <SelectionDialog
+      title="Select Sports"
+      list={sports}
+      saveChange={(list) => console.log(list)}
+      state={{ open, setOpen }}
+    />
+  );
+};
