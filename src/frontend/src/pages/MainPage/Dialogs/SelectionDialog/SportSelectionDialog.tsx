e7cd10c0649eb68654b1a561799946c128de6789
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-08-02 14:36:51 +0200

    feat: copied saveListsToCoockies to SportSelectionDialog

--- src/frontend/src/pages/MainPage/Dialogs/SelectionDialog/SportSelectionDialog.tsx
+++ src/frontend/src/pages/MainPage/Dialogs/SelectionDialog/SportSelectionDialog.tsx
@@ -2,6 +2,9 @@ import { useMemo } from 'react';
 import { useSportResponseStore } from '../../../../zustand/sportResponseStore';
 import { DialogStateProps, SelectionDialog } from './SelectionDialog';
 import { SportEntry } from '../../QuickActions/SearchModal';
+import { UserPreferences } from '../../../../models/Preferences';
+import usePreferenceStore from '../../../../zustand/PreferenceStore';
+import { setCookie } from '../../../../utils/cookies';
 
 export interface SportSelectionDialogProps {
   state: DialogStateProps;
@@ -11,16 +14,32 @@ export const SportSelectionDialog: React.FC<SportSelectionDialogProps> = ({
   state: { open, setOpen },
 }) => {
   const { sportResponse } = useSportResponseStore();
+  const { preferences, setPreferences } = usePreferenceStore();
   const sports: SportEntry[] = useMemo(() => {
     if (!sportResponse || !sportResponse.sports) return [];
     return Object.keys(sportResponse.sports).map((s) => new SportEntry(s));
   }, [sportResponse]);
 
+  // save newly sorted list to cookies
+  const saveListsToCookies = (targetList: string[]) => {
+    const uiList = targetList.length > 0 ? targetList : null;
+
+    const newPreferences: UserPreferences = {
+      ...preferences,
+      ui: {
+        ...preferences.ui,
+        displayedSports: uiList,
+      },
+    };
+    setPreferences(newPreferences);
+    setCookie('preferences', JSON.stringify(newPreferences), 999);
+  };
+
   return (
     <SelectionDialog
       title="Select Sports"
       list={sports}
-      saveChange={(list) => console.log(list)}
+      saveChange={(list) => saveListsToCookies(list.map((entry) => entry.name))}
       state={{ open, setOpen }}
     />
   );
