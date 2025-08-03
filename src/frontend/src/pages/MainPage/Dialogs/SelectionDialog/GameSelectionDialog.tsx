661c634e3b5838a6ceb34294d299f061083a530c
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-08-03 11:24:24 +0200

    ui: added Dialog for selecting games by reusing SelectionDialog

---
+++ src/frontend/src/pages/MainPage/Dialogs/SelectionDialog/GameSelectionDialog.tsx
@@ -0,0 +1,58 @@
+import { useMemo } from 'react';
+import { useSportResponseStore } from '../../../../zustand/sportResponseStore';
+import { DialogStateProps, SelectionDialog } from './SelectionDialog';
+import {
+  GameEntry,
+  SearchEntry,
+  SportEntry,
+} from '../../QuickActions/SearchEntry';
+import { UserPreferences } from '../../../../models/Preferences';
+import usePreferenceStore from '../../../../zustand/PreferenceStore';
+import { setCookie } from '../../../../utils/cookies';
+import { getValidGames } from '../../GameSelect';
+import { getThemeNames } from '../../../../zustand/useThemeStore';
+
+export interface GameSelectionDialogProps {
+  state: DialogStateProps;
+}
+
+export const GameSelectionDialog: React.FC<GameSelectionDialogProps> = ({
+  state: { open, setOpen },
+}) => {
+  const { preferences, setPreferences } = usePreferenceStore();
+  const games: GameEntry[] = useMemo(() => {
+    var preferredEntries =
+      preferences.ui.displayedGames?.map(
+        (s) => new GameEntry(s.name, s.isDisplayed)
+      ) ?? [];
+
+    const notSelected = getThemeNames()
+      .filter((game) => !preferredEntries.some((entry) => entry.name === game))
+      .map((game) => new GameEntry(game, false));
+    return [...preferredEntries, ...notSelected];
+  }, [preferences]);
+
+  // save newly sorted list to cookies
+  const saveListsToCookies = (targetList: SearchEntry[]) => {
+    const uiList = targetList.length > 0 ? targetList : null;
+
+    const newPreferences: UserPreferences = {
+      ...preferences,
+      ui: {
+        ...preferences.ui,
+        displayedGames: uiList,
+      },
+    };
+    setPreferences(newPreferences);
+    setCookie('preferences', JSON.stringify(newPreferences), 999);
+  };
+
+  return (
+    <SelectionDialog
+      title="Game Overview"
+      list={games}
+      saveChange={saveListsToCookies}
+      state={{ open, setOpen }}
+    />
+  );
+};
