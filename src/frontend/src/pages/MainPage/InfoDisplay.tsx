6a80e31290370f3deb5dc8b475d4303303b742f7
kurama <paul.zenker.pz@gmail.com>
2025-08-08 15:49:06 +0200

    feat: dynamic duration support added

--- src/frontend/src/pages/MainPage/InfoDisplay.tsx
+++ src/frontend/src/pages/MainPage/InfoDisplay.tsx
@@ -7,58 +7,50 @@ import {
   Box,
   LinearProgress,
 } from '@mui/material';
-import useInfoStore from '../../zustand/InfoStore';
+import useInfoStore, { SnackbarUpdateImpl } from '../../zustand/InfoStore';
 import { useBreakpoint } from '../../hooks/useBreakpoint';
 import { set } from 'zod';
 
 const InfoDisplay: React.FC = () => {
-  const { Message, setMessage: setErrorMessage } = useInfoStore();
+  const { Message } = useInfoStore();
   const [open, setOpen] = useState(false);
   const [progress, setProgress] = useState(0); // 0 to 100
-  const DEFAULT_DURATION = 6000;
   const INTERVAL = 100;
   const { isMobile } = useBreakpoint();
 
   useEffect(() => {
-    if (!open) {
-      return;
+    if (open) {
+      const steps = Message.getDurationMs() / INTERVAL;
+      const increment = 100 / steps;
+
+      // timer which increases
+      const timer = window.setInterval(() => {
+        setProgress((prev) => {
+          const next = prev + increment;
+          if (next >= 100) {
+            clearInterval(timer);
+            window.setTimeout(() => {
+              // this delay is used, that the loading bar
+              // actually reaches the end due to animations
+              setOpen(false);
+            }, INTERVAL * 3);
+            return 100;
+          }
+          return next;
+        });
+      }, INTERVAL);
+      // close when timer is done
+
+      return () => clearInterval(timer);
     }
-
-    const steps = (Message.duration ?? DEFAULT_DURATION) / INTERVAL;
-    const increment = 100 / steps;
-
-    // timer which increases
-    const timer = window.setInterval(() => {
-      setProgress((prev) => {
-        const next = prev + increment;
-        if (next >= 100) {
-          clearInterval(timer);
-          window.setTimeout(() => {
-            setOpen(false);
-          }, INTERVAL);
-          return 100;
-        }
-        return next;
-      });
-    }, INTERVAL);
-    // close when timer is done
-
-    return () => clearInterval(timer);
-  }, [open]);
+  }, [open, Message]);
 
   // Monitor error message changes
   useEffect(() => {
     if (Message && Message.message !== '') {
       setOpen(true);
-    } else {
-      setOpen(false);
     }
   }, [Message]);
-
-  // Slide animation
-  // const SlideTransition = (props: SlideProps) => {
-  //   return <Slide {...props} direction="down" />;
-  // };
 
   const handleClose = (
     _event?: React.SyntheticEvent | Event,
@@ -68,24 +60,14 @@ const InfoDisplay: React.FC = () => {
     setOpen(false);
   };
 
-  // Handle cleanup after animation
-  // useless?
-  const handleExited = () => {
-    // setErrorMessage({
-    //   message: '',
-    //   severity: 'info',
-    // });
-    // setProgress(0); // Reset progress bar
-  };
-
   return (
     <Snackbar
       open={open}
-      autoHideDuration={Message.duration ?? DEFAULT_DURATION}
       onClose={handleClose}
       anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
       key={Message.message} // This key is important for re-triggering with the same message
-      slotProps={{ transition: { onExited: handleExited } }}
+      //slotProps={{ transition: { onExited: handleExited } }}
+      sx={{ zIndex: 9999 }}
     >
       <Typography variant="h4" component="div">
         <Alert severity={Message.severity}>
