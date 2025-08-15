e1425190cd97a68dd988c1f6f6aa24994e6d250c
kurama <paul.zenker.pz@gmail.com>
2025-08-15 15:52:48 +0200

    feat: test for SportCalculator

---
+++ src/frontend/src/utils/SportCalculator.test.ts
@@ -0,0 +1,79 @@
+import { describe } from 'node:test';
+import { UserPreferences } from '../models/Preferences';
+import { GetSportsResponse } from '../models/Sport';
+import {
+  BaseSportsCalculatorDecorator,
+  DeathDecorator,
+  ExactlyOneDecorator,
+  HumanLockDecorator,
+  MultiplierDecorator,
+  OverrideSportDecorator,
+  PreferenceRespectingDefaultSportsCalculator,
+  SportsCalculator,
+} from './SportCalculator';
+import { assert } from 'console';
+
+const testingPreferences: UserPreferences = {
+  game_overrides: [],
+  max_deaths: 10,
+  multipliers: [
+    { game: 'league', multiplier: 3, sport: 'leg-raises' },
+    { game: 'overwatch', multiplier: 2, sport: null },
+  ],
+  other: {
+    instant_open_modal: false,
+  },
+  sport_specific: {
+    plank: {
+      seconds: 50,
+    },
+  },
+  ui: {
+    displayedGames: [],
+    displayedSports: [],
+  },
+};
+
+const testingSportResponse: GetSportsResponse = {
+  games: {
+    overwatch: 2,
+    league: 3,
+  },
+  sports: {
+    'push-up': 4,
+    'leg-raises': 5,
+  },
+};
+
+export const buildDecoratorStack = (): SportsCalculator => {
+  const BASE_SETTINGS = testingSportResponse;
+  const preferences = testingPreferences;
+
+  // base for calculating default values with respecting the users overrides
+  var base: SportsCalculator = new PreferenceRespectingDefaultSportsCalculator(
+    BASE_SETTINGS,
+    preferences
+  );
+
+  // custom per game per sport overrides
+  base = new OverrideSportDecorator(base, preferences.game_overrides);
+
+  // add DeathDecorator, to wrap the output with the death amount
+  base = new DeathDecorator(base);
+
+  // custom multipliers, either global or per game and sport
+  base = new MultiplierDecorator(base, preferences.multipliers);
+
+  // custom decorator for planks
+  base = new HumanLockDecorator(base);
+
+  return base;
+};
+
+describe('SportCalculator', () => {
+  const calc = buildDecoratorStack();
+
+  it('calculates bare metal', () => {
+    expect(calc.calculate_amount('push-up', 'league', 3)).toBe(60); // 3 * 4 * 5 = 60
+  });
+});
