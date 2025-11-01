fad4b554d70b66992efdd3c9ae08208072e94426
kurama <paul.zenker.pz@gmail.com>
2025-11-01 18:31:30 +0100

    feat: added Step class to represent one step of the DurationCalculator

--- src/frontend/src/utils/durationCalculator.ts
+++ src/frontend/src/utils/durationCalculator.ts
@@ -3,14 +3,12 @@
 export class DurationCalculator {
   total_ms: number;
   steps: number;
-
-  // 0 - (steps - 1)
-  current_step: number;
+  current_step: Step;
 
   constructor(total_ms: number, steps: number) {
     this.total_ms = total_ms;
     this.steps = steps;
-    this.current_step = 0;
+    this.current_step = new Step(0, this);
   }
 
   // The duration of one step/interval in ms
@@ -22,5 +20,30 @@ export class DurationCalculator {
   // percentage (0 - 1) of one step.
   get_step_percentage(): number {
     return this.steps / 100;
+  }
+
+  // incement step by one
+  next_step(): void {
+    if (this.current_step.n >= this.steps - 1) {
+      throw new Error('cant increment to next step. Already reached last step');
+    }
+    this.current_step = new Step(this.current_step.n + 1, this);
+  }
+}
+
+// Represents one (most likely `currnent`) step for
+// Duration calculator
+export class Step {
+  n: number;
+  duration_calculator: DurationCalculator;
+  constructor(n: number, duration_calculator: DurationCalculator) {
+    this.n = n;
+    this.duration_calculator = duration_calculator;
+  }
+
+  // calculates the percentage of this step compared to the
+  // total from the DurationCalculator
+  get_percentage(): number {
+    return this.duration_calculator.get_step_percentage() * (this.n + 1);
   }
 }
