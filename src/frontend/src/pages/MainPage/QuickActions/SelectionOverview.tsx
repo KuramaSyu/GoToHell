48fd1cd50baf45dd7c8de571dc5548a29af3bdd4
kurama <paul.zenker.pz@gmail.com>
2026-01-10 17:46:26 +0100

    feat: improved KeyValuePill

--- src/frontend/src/pages/MainPage/QuickActions/SelectionOverview.tsx
+++ src/frontend/src/pages/MainPage/QuickActions/SelectionOverview.tsx
@@ -1,7 +1,8 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, darken, Stack, Typography } from '@mui/material';
 import { useSportStore } from '../../../useSportStore';
 import { useThemeStore } from '../../../zustand/useThemeStore';
 import { useDeathAmountStore } from '../NumberSlider';
+import { blendWithContrast } from '../../../utils/blendWithContrast';
 
 /**
  * Component to display death amount, theme and sport
@@ -13,7 +14,7 @@ export const SelectionOverview: React.FC = () => {
 
   return (
     <Stack direction="row" width={'100%'} justifyContent="space-around" mb={2}>
-      <Box>Theme: {String(theme.custom.longName)}</Box>
+      <KeyValuePill key="Theme" value={theme.custom.themeName} />
       <Box>Sport: {currentSport.sport ?? 'Unknown'}</Box>
       <Box>Exercise Amount: {amount}</Box>
     </Stack>
@@ -27,9 +28,28 @@ export interface KeyValuePillProps {
 export const KeyValuePill: React.FC<KeyValuePillProps> = ({ key, value }) => {
   const { theme } = useThemeStore();
   return (
-    <Stack color={'primary'}>
-      <Typography>key</Typography>
-      <Box sx={{ zIndex: 2, border: theme.shape.borderRadius }}>value</Box>
+    <Stack
+      sx={{
+        backgroundColor: darken(theme.palette.muted.main, 0.2),
+        pl: 1,
+        borderRadius: theme.shape.borderRadius,
+        alignItems: 'center',
+        gap: 1,
+        color: theme.palette.text.primary,
+      }}
+      direction={'row'}
+    >
+      <Typography textTransform={'capitalize'}>{key}</Typography>
+      <Box
+        sx={{
+          zIndex: 2,
+          borderRadius: theme.shape.borderRadius,
+          background: theme.palette.muted.main,
+          p: 1,
+        }}
+      >
+        {value}
+      </Box>
     </Stack>
   );
 };
