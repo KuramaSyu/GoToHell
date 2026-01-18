19ca9e841ee5a6d39e98dabf8a5204e815aca529
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-01-18 17:21:39 +0100

    chore: removed debug print

--- src/frontend/src/pages/MainPage/AmountDisplay.tsx
+++ src/frontend/src/pages/MainPage/AmountDisplay.tsx
@@ -1,43 +1,43 @@
-import { Box, lighten, Typography, useMediaQuery } from '@mui/material';
-import { useSportStore } from '../../useSportStore';
-import { useDeathAmountStore } from './NumberSlider';
-import { NUMBER_FONT } from '../../statics';
-import React, { ReactElement, useMemo, useState } from 'react';
+import { Box, lighten, Typography, useMediaQuery } from "@mui/material";
+import { useSportStore } from "../../useSportStore";
+import { useDeathAmountStore } from "./NumberSlider";
+import { NUMBER_FONT } from "../../statics";
+import React, { ReactElement, useMemo, useState } from "react";
 import {
   DefaultSportsCalculator,
   ExactlyOneDecorator,
   MultiplierDecorator,
   OverrideSportDecorator,
   SportsCalculator,
-} from '../../utils/SportCalculator';
-import useCalculatorStore from '../../zustand/CalculatorStore';
-import Star from '../../components/Shapes';
-import { useThemeStore } from '../../zustand/useThemeStore';
-import { darken } from '@mui/material/styles';
-import usePreferenceStore from '../../zustand/PreferenceStore';
-import { Timedelta, Unit, unitToString } from '../../utils/Timedelta';
-import { GameSelectionMap } from '../../utils/data/Sports';
-import { useBreakpoint } from '../../hooks/useBreakpoint';
-import { PopNumber } from './PopNumber';
-import { styled } from '@mui/material';
-import { motion } from 'framer-motion';
-import { getSportDescription } from '../../utils/DescriptionProvider';
+} from "../../utils/SportCalculator";
+import useCalculatorStore from "../../zustand/CalculatorStore";
+import Star from "../../components/Shapes";
+import { useThemeStore } from "../../zustand/useThemeStore";
+import { darken } from "@mui/material/styles";
+import usePreferenceStore from "../../zustand/PreferenceStore";
+import { Timedelta, Unit, unitToString } from "../../utils/Timedelta";
+import { GameSelectionMap } from "../../utils/data/Sports";
+import { useBreakpoint } from "../../hooks/useBreakpoint";
+import { PopNumber } from "./PopNumber";
+import { styled } from "@mui/material";
+import { motion } from "framer-motion";
+import { getSportDescription } from "../../utils/DescriptionProvider";
 
-export const BIG_NUMBER_SIZE_MOBILE = '6vh';
-export const BIG_NUMBER_SIZE_DESKTOP = '12vh';
-export const AMOUNT_DISPLAY_TITLE_SX = { fontSize: '3vh' };
-export const AMOUNT_DISPLAY_CONENT_SX = { fontSize: '2vh' };
+export const BIG_NUMBER_SIZE_MOBILE = "6vh";
+export const BIG_NUMBER_SIZE_DESKTOP = "12vh";
+export const AMOUNT_DISPLAY_TITLE_SX = { fontSize: "3vh" };
+export const AMOUNT_DISPLAY_CONENT_SX = { fontSize: "2vh" };
 export const AMOUNT_DISPLAY_CONTENT_BOX_SX = {
-  display: 'flex',
-  flexDirection: 'column',
-  height: '100%',
-  alignItems: 'left',
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+  alignItems: "left",
   mt: { xs: -2, md: -4 }, // Remove weird padding from font
 };
 
 export function getAnimatedNumberSize(isMobile: boolean, numberKind: string) {
-  if (numberKind === 'time') {
-    return isMobile ? '4vh' : '8vh';
+  if (numberKind === "time") {
+    return isMobile ? "4vh" : "8vh";
   }
   return isMobile ? BIG_NUMBER_SIZE_MOBILE : BIG_NUMBER_SIZE_DESKTOP;
 }
@@ -59,7 +59,7 @@ const NumberDisplay: React.FC<SportServiceProps> = ({
       stiffness={1000}
       damping={300}
       mass={1}
-      key={'AnimatedNumber'}
+      key={"AnimatedNumber"}
       style={{}}
     />
   );
@@ -74,11 +74,11 @@ const TimeDisplay: React.FC<SportServiceProps> = ({
   const minutes = timedelta.minutes();
   const hours = timedelta.hours();
   const biggestUnit = timedelta.biggestUnit();
-  const numberKind = 'time';
+  const numberKind = "time";
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+    <Box sx={{ display: "flex", alignItems: "center" }}>
       {biggestUnit >= Unit.hours ? (
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           <PopNumber
             value={hours}
             font={NUMBER_FONT}
@@ -96,7 +96,7 @@ const TimeDisplay: React.FC<SportServiceProps> = ({
         </Box>
       ) : null}
       {biggestUnit >= Unit.minutes ? (
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           <PopNumber
             value={minutes}
             font={NUMBER_FONT}
@@ -116,7 +116,7 @@ const TimeDisplay: React.FC<SportServiceProps> = ({
       ) : null}
 
       {/* seconds are always shown */}
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
         <PopNumber
           value={seconds}
           font={NUMBER_FONT}
@@ -132,11 +132,11 @@ const TimeDisplay: React.FC<SportServiceProps> = ({
 };
 
 export function getDisplayComponent(
-  sport: string | undefined
+  sport: string | undefined,
 ): React.FC<SportServiceProps> {
   switch (sport) {
-    case 'plank':
-    case 'workout':
+    case "plank":
+    case "workout":
       return TimeDisplay;
     default:
       return NumberDisplay;
@@ -158,38 +158,37 @@ export const AmountDisplay = () => {
   const computedValue = calculator.calculate_amount(
     currentSport.sport!,
     currentSport.game!,
-    amount
+    amount,
   );
 
   const DisplayComponent = getDisplayComponent(currentSport.sport);
-  // currentSport.game_multiplier! * amount * currentSport.sport_multiplier!;
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyItems: 'center',
+        display: "flex",
+        flexDirection: "column",
+        justifyItems: "center",
         alignItems: {
-          xs: 'right',
-          md: 'right',
-          lg: 'center',
+          xs: "right",
+          md: "right",
+          lg: "center",
         },
       }}
     >
       <Box
         sx={{
-          display: 'flex',
+          display: "flex",
           flexDirection: {
-            xs: 'column',
-            md: 'column',
-            lg: 'row',
+            xs: "column",
+            md: "column",
+            lg: "row",
           },
-          justifyItems: 'center',
+          justifyItems: "center",
           alignItems: {
-            xs: 'right',
-            md: 'right',
-            lg: 'center',
+            xs: "right",
+            md: "right",
+            lg: "center",
           },
           fontFamily: NUMBER_FONT,
         }}
@@ -202,10 +201,10 @@ export const AmountDisplay = () => {
           <DisplayComponent computedValue={computedValue} isMobile={isMobile} />
         </Box>
         <Box sx={AMOUNT_DISPLAY_CONTENT_BOX_SX}>
-          <Typography sx={AMOUNT_DISPLAY_TITLE_SX} fontFamily={'inherit'}>
+          <Typography sx={AMOUNT_DISPLAY_TITLE_SX} fontFamily={"inherit"}>
             {getSportDescription(currentSport.sport, computedValue)}
           </Typography>
-          <Typography sx={AMOUNT_DISPLAY_CONENT_SX} fontFamily={'inherit'}>
+          <Typography sx={AMOUNT_DISPLAY_CONENT_SX} fontFamily={"inherit"}>
             to do now
           </Typography>
         </Box>
