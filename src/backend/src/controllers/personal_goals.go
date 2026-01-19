33cfaacdaf8e76f74cd8e1227e792a837c997979
kurama <paul.zenker.pz@gmail.com>
2026-01-19 17:07:48 +0100

    feat: started creating middleware for personal goals
    this is unfinished
    #38

---
+++ src/backend/src/controllers/personal_goals.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	. "github.com/KuramaSyu/GoToHell/src/backend/src/api/repositories"
+	"github.com/KuramaSyu/GoToHell/src/backend/src/db"
+	. "github.com/KuramaSyu/GoToHell/src/backend/src/models"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+// GetPersonalGoalsReply is the reply sent when doing [get] /{user_id}/goals
+// swagger:model GetPersonalGoalsReply
+type GetPersonalGoalsReply struct {
+	Data []PersonalGoal `json:"data"`
+}
+
+func NewPersonalGoalsController(database *gorm.DB) *PersonalGoalsController {
+	return &PersonalGoalsController{
+		repo: db.NewPersonalGoalsRepository(database),
+	}
+}
+
+// PersonalGoalsController manages personal goals endpoints.
+type PersonalGoalsController struct {
+	repo PersonalGoalsRepository
+}
+
+// returns all PersonalGoal records for the user
+// @Summary Get all PersonalGoal records for the requested user
+// @Tags PersonalGoals
+// @Produce json
+// @Accept json
+// @Success 200 {object} GetPersonalGoalsReply
+// @Failure 400 {object} ErrorReply
+// @Router /{user_id}/goals [get]
+func (pgc *PersonalGoalsController) Get(c *gin.Context) {
+	requested_user_id := c.Param("user_id")
+	_, status, err := UserFromSession(c)
+	if err != nil {
+		SetGinError(c, status, err)
+		return
+	}
+}
