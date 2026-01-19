32e4c0f975c56fd66c7922d5f64b5e06f8d979a4
kurama <paul.zenker.pz@gmail.com>
2026-01-19 16:27:06 +0100

    chore: moved user connection down #30

--- src/backend/src/models/personal_goals.go
+++ src/backend/src/models/personal_goals.go
@@ -11,12 +11,12 @@ const (
 // SQL Table representing a friendship between person A <RequesterID> and person B <RecipientID>.
 // The status <Status> of the friendship is either pending, accepted or blocked
 type PersonalGoal struct {
-	ID     Snowflake `gorm:"primaryKey" json:"id"`
-	UserID Snowflake `gorm:"not null;index" json:"user_id"`
-
-	// just a constraint to use UserID as foreign key
-	User      User          `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE"`
+	ID        Snowflake     `gorm:"primaryKey" json:"id"`
+	UserID    Snowflake     `gorm:"not null;index" json:"user_id"`
 	Amount    int           `json:"amount"`
 	Frequency TimeFrequency `json:"frequency"`
 	Sport     string        `json:"sport"`
+
+	// just a constraint to use UserID as foreign key
+	User User `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE"`
 }
