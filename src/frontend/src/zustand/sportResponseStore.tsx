2544d6913cb362dd8e3ce9bc4f72452a4bf761ab
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-01-25 20:03:48 +0100

    api: added getSportEntryMap

--- src/frontend/src/zustand/sportResponseStore.tsx
+++ src/frontend/src/zustand/sportResponseStore.tsx
@@ -2,6 +2,10 @@ import { create } from 'zustand';
 import { GetSportsResponse } from '../models/Sport';
 import multiplierData from '../utils/data/Multipliers.json';
 import { Multiplier } from '../models/Preferences';
+import {
+  SearchEntry,
+  SportEntry,
+} from '../pages/MainPage/QuickActions/SearchEntry';
 
 const data: GetSportsResponse = multiplierData;
 interface SportResponseState {
@@ -9,15 +13,21 @@ interface SportResponseState {
   setSportResponse: (sportResponse: GetSportsResponse) => void;
   emptySportsResponse: () => GetSportsResponse;
   getSportMultiplier: (sport: string) => Multiplier;
+  getSportEntryMap: () => Map<string, SearchEntry>;
 }
+
+let sportEntryMap: Map<string, SearchEntry> | null = null;
 
 export const useSportResponseStore = create<SportResponseState>((set) => ({
   sportResponse: multiplierData,
+
   setSportResponse: (sportResponse: GetSportsResponse) =>
     set({ sportResponse: sportResponse }),
+
   emptySportsResponse: () => {
     return { games: {}, sports: {} };
   },
+
   getSportMultiplier: (sport: string) => {
     const { sports } = useSportResponseStore.getState().sportResponse;
     if (sports === undefined) {
@@ -33,5 +43,21 @@ export const useSportResponseStore = create<SportResponseState>((set) => ({
       sport: sport,
     };
     return multiplier;
+  },
+
+  getSportEntryMap: () => {
+    if (
+      sportEntryMap
+      // && Array.from(sportEntryMap.keys()).length >=
+      //   Object.keys(multiplierData.sports).length
+    ) {
+      return sportEntryMap;
+    }
+    sportEntryMap = Object.keys(multiplierData.sports)
+      .map((s) => new SportEntry(s))
+      .reduce((map, entry) => {
+        return map.set(entry.name, entry);
+      }, new Map<string, SearchEntry>());
+    return sportEntryMap;
   },
 }));
