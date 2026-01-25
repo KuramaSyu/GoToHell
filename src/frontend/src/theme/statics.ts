89025ff9b36e0d7c678dcbc1a0bc78d6cecdd6ac
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-01-25 23:20:15 +0100

    chore: added BoxHoverPropsTopBar

--- src/frontend/src/theme/statics.ts
+++ src/frontend/src/theme/statics.ts
@@ -14,3 +14,12 @@ export const BoxElevation2 = (theme: CustomTheme) => ({
   backgroundColor: alpha(theme.palette.muted.dark, 0.3),
   backdropFilter: 'blur(8px)',
 });
+
+export const BoxHoverPropsTopBar = (theme: CustomTheme) => ({
+  transition: theme.transitions.create(['background-color'], {
+    duration: theme.transitions.duration.short,
+  }),
+  '&:hover': {
+    backgroundColor: alpha(theme.blendAgainstContrast('secondary', 0.2), 0.8),
+  },
+});
