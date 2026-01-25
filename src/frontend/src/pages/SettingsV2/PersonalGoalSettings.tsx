4d245d2a104530f0368249237e18214c91ee682d
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-01-25 20:31:41 +0100

    ux: added description

--- src/frontend/src/pages/SettingsV2/PersonalGoalSettings.tsx
+++ src/frontend/src/pages/SettingsV2/PersonalGoalSettings.tsx
@@ -30,7 +30,7 @@ import {
 } from '../../utils/api/PersonalGoalsApi';
 import useInfoStore, { SnackbarUpdateImpl } from '../../zustand/InfoStore';
 import DeleteIcon from '@mui/icons-material/Delete';
-
+import FlagIcon from '@mui/icons-material/Flag';
 export const PersonalGoalSettings: React.FC = () => {
   const { personalGoalsList } = usePersonalGoalsStore();
   const [amountStr, setAmountStr] = useState('1');
@@ -100,114 +100,121 @@ export const PersonalGoalSettings: React.FC = () => {
   };
 
   return (
-    <TableContainer component={Paper} elevation={0}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Typography variant='h6'>Exercises</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant='h6'>Sport</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant='h6'>Frequency</Typography>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {personalGoalsList.map((goal) => (
-            <TableRow
-              sx={{
-                '& .delete-button': { visibility: 'hidden' },
-                '&:hover .delete-button': { visibility: 'visible' },
-              }}
-            >
+    <Stack direction='column' gap={2} mb={10}>
+      <Typography variant='body1'>
+        {`Manage your personal goals here. For example 10 Push-Ups per day. You will see the Goal Tracker`}{' '}
+        (<FlagIcon />){' '}
+        {` in the top bar, showing the Percentage (%) of your goal completed.`}
+      </Typography>
+      <TableContainer component={Paper} elevation={0}>
+        <Table>
+          <TableHead>
+            <TableRow>
               <TableCell>
-                <Typography>{goal.amount}</Typography>
+                <Typography variant='h6'>Exercises</Typography>
               </TableCell>
               <TableCell>
-                <Typography>
-                  {sportEntries.get(goal.sport)?.displayName() ?? 'unknown'}
-                </Typography>
+                <Typography variant='h6'>Sport</Typography>
               </TableCell>
               <TableCell>
-                <Stack
-                  direction='row'
-                  sx={{
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                  }}
-                >
-                  <Typography>{goal.frequency}</Typography>
-                  <IconButton
-                    className='delete-button'
-                    color='error'
-                    onClick={() => deleteDailyGoal(goal.id)}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </Stack>
+                <Typography variant='h6'>Frequency</Typography>
               </TableCell>
             </TableRow>
-          ))}
+          </TableHead>
+          <TableBody>
+            {personalGoalsList.map((goal) => (
+              <TableRow
+                sx={{
+                  '& .delete-button': { visibility: 'hidden' },
+                  '&:hover .delete-button': { visibility: 'visible' },
+                }}
+              >
+                <TableCell>
+                  <Typography>{goal.amount}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>
+                    {sportEntries.get(goal.sport)?.displayName() ?? 'unknown'}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Stack
+                    direction='row'
+                    sx={{
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Typography>{goal.frequency}</Typography>
+                    <IconButton
+                      className='delete-button'
+                      color='error'
+                      onClick={() => deleteDailyGoal(goal.id)}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </Stack>
+                </TableCell>
+              </TableRow>
+            ))}
 
-          <TableRow>
-            <TableCell>
-              <Stack direction='row' gap={2}>
-                <Button
-                  variant='contained'
-                  startIcon={<AddIcon />}
-                  disabled={!isEverythingSelected()}
-                  onClick={() => uploadDailyGoal()}
-                >
-                  <Typography>Add</Typography>
-                </Button>
-                <TextField
-                  value={amountStr}
-                  onChange={(e) => setAmountStr(e.target.value)}
-                  id='outlined-basic'
-                  label='Amount'
-                  type='number'
-                  variant='outlined'
-                  helperText={
-                    !isNumeric(amountStr) ? 'e.g. 30, 2, or 42' : null
-                  }
-                  error={!isNumeric(amountStr)}
+            <TableRow>
+              <TableCell>
+                <Stack direction='row' gap={2}>
+                  <Button
+                    variant='contained'
+                    startIcon={<AddIcon />}
+                    disabled={!isEverythingSelected()}
+                    onClick={() => uploadDailyGoal()}
+                  >
+                    <Typography>Add</Typography>
+                  </Button>
+                  <TextField
+                    value={amountStr}
+                    onChange={(e) => setAmountStr(e.target.value)}
+                    id='outlined-basic'
+                    label='Amount'
+                    type='number'
+                    variant='outlined'
+                    helperText={
+                      !isNumeric(amountStr) ? 'e.g. 30, 2, or 42' : null
+                    }
+                    error={!isNumeric(amountStr)}
+                  />
+                </Stack>
+              </TableCell>
+              <TableCell>
+                <Autocomplete
+                  value={selectedSport}
+                  onChange={(_, newValue) => {
+                    setSelectedSport(newValue || '');
+                  }}
+                  disablePortal
+                  options={Array.from(sportEntries.values()).map((e) =>
+                    e.displayName(),
+                  )}
+                  renderInput={(params) => (
+                    <TextField {...params} label='Select a Sport' />
+                  )}
                 />
-              </Stack>
-            </TableCell>
-            <TableCell>
-              <Autocomplete
-                value={selectedSport}
-                onChange={(_, newValue) => {
-                  setSelectedSport(newValue || '');
-                }}
-                disablePortal
-                options={Array.from(sportEntries.values()).map((e) =>
-                  e.displayName(),
-                )}
-                renderInput={(params) => (
-                  <TextField {...params} label='Select a Sport' />
-                )}
-              />
-            </TableCell>
-            <TableCell>
-              <Autocomplete
-                value={frequency}
-                onChange={(_, newValue) => {
-                  setFrequency((newValue as GoalFrequency) || 'daily');
-                }}
-                disablePortal
-                options={['daily', 'weekly', 'monthly']}
-                renderInput={(params) => (
-                  <TextField {...params} label='Select a frequency' />
-                )}
-              />
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+              </TableCell>
+              <TableCell>
+                <Autocomplete
+                  value={frequency}
+                  onChange={(_, newValue) => {
+                    setFrequency((newValue as GoalFrequency) || 'daily');
+                  }}
+                  disablePortal
+                  options={['daily', 'weekly', 'monthly']}
+                  renderInput={(params) => (
+                    <TextField {...params} label='Select a frequency' />
+                  )}
+                />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Stack>
   );
 };
