5313b387832820d2eb015144cbd6deb7b17be4f5
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-01-25 21:47:17 +0100

    chore: added BoxElevation0

--- src/frontend/src/theme/statics.ts
+++ src/frontend/src/theme/statics.ts
@@ -1,12 +1,16 @@
-import { alpha } from "@mui/material";
-import { CustomTheme } from "./customTheme";
+import { alpha } from '@mui/material';
+import { CustomTheme } from './customTheme';
+
+export const BoxElevation0 = (theme: CustomTheme) => ({
+  backgroundColor: theme.palette.muted.dark,
+});
 
 export const BoxElevation1 = (theme: CustomTheme) => ({
   backgroundColor: alpha(theme.palette.muted.dark, 0.6),
-  backdropFilter: "blur(16px)",
+  backdropFilter: 'blur(16px)',
 });
 
 export const BoxElevation2 = (theme: CustomTheme) => ({
   backgroundColor: alpha(theme.palette.muted.dark, 0.3),
-  backdropFilter: "blur(8px)",
+  backdropFilter: 'blur(8px)',
 });
