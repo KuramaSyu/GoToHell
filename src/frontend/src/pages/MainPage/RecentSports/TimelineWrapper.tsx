93b46bf8a067f611b1c77372fb6c7963215bbeb6
kurama <paul.zenker.pz@gmail.com>
2026-01-31 12:08:03 +0100

    perf: use of own time based Progressbar

--- src/frontend/src/pages/MainPage/RecentSports/TimelineWrapper.tsx
+++ src/frontend/src/pages/MainPage/RecentSports/TimelineWrapper.tsx
@@ -1,28 +1,19 @@
-import { Box, LinearProgress } from '@mui/material';
-import { memo, useEffect, useState } from 'react';
+import { Box, LinearProgress, Theme } from '@mui/material';
+import { ResponsiveStyleValue } from '@mui/system';
+import React, { memo, useEffect, useState } from 'react';
 import { StreakTimeline } from './StreakTimeline';
 import { SportsTimeline } from './Timeline';
 
 const TOTAL_DURATION_MS = 5000;
-
 const MemoStreakTimeline = memo(StreakTimeline);
 
 export const TimelineWrapper: React.FC = () => {
-  // does not actually represent the progress.
-  // it instantly goes to 100, so the
-  // bar starts a 5s animation from 0 -> 100
-  const [progress, setProgress] = useState(0);
-
   // is set after 5s
   const [progressCompleted, setProgressCompleted] = useState(false);
 
-  // on load: set progress to 100
   // and start 5 second timer to set progressCompleted to true
   useEffect(() => {
-    if (progress === 100) return;
-
     // Start new timer
-    setProgress(100);
     var timer = window.setInterval(() => {
       setProgressCompleted(true);
     }, TOTAL_DURATION_MS);
@@ -35,21 +26,44 @@ export const TimelineWrapper: React.FC = () => {
     <Box>
       {!progressCompleted ? (
         <Box>
-          <LinearProgress
-            variant='determinate'
-            value={progress}
-            sx={{
-              height: 10,
-              '& .MuiLinearProgress-bar': {
-                transition: `transform ${TOTAL_DURATION_MS}ms linear`,
-              },
-            }}
-          />
+          <SimpleTimeBasedProgressBar durationMs={TOTAL_DURATION_MS} />
           <MemoStreakTimeline />
         </Box>
       ) : (
         <SportsTimeline></SportsTimeline>
       )}
     </Box>
+  );
+};
+
+export interface SimpleTimeBasedProgressBarProps {
+  durationMs: number;
+  barColor?:
+    | ResponsiveStyleValue<string>
+    | ((theme: Theme) => ResponsiveStyleValue<string>)
+    | null;
+}
+
+/**
+ * Makes a simple progress bar from 0% width to 100% width
+ * over the given duration in milliseconds
+ * @param durationMs Duration in milliseconds
+ * @param backgroundColor Background color of the progress bar
+ */
+export const SimpleTimeBasedProgressBar: React.FC<
+  SimpleTimeBasedProgressBarProps
+> = ({ durationMs, barColor }) => {
+  return (
+    <Box
+      sx={{
+        height: 12,
+        width: '0%',
+        backgroundColor: barColor ?? 'primary.main',
+        animation: `progressBar ${durationMs}ms linear forwards`,
+        '@keyframes progressBar': {
+          to: { width: '100%' },
+        },
+      }}
+    ></Box>
   );
 };
