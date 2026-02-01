c360ab092b364fc23840ce7df140820d76018aa1
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-02-01 03:46:31 +0100

    chore: set cookies in perferences

--- src/frontend/src/zustand/PreferenceStore.tsx
+++ src/frontend/src/zustand/PreferenceStore.tsx
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { defaultPreferences, UserPreferences } from '../models/Preferences';
+import { setCookie } from '../utils/cookies';
 
 interface PreferencesState {
   preferences: UserPreferences;
@@ -10,8 +11,10 @@ interface PreferencesState {
 const usePreferenceStore = create<PreferencesState>((set) => ({
   preferences: defaultPreferences(),
   preferencesLoaded: false,
-  setPreferences: (preferences) =>
-    set({ preferences: preferences, preferencesLoaded: true }),
+  setPreferences: (preferences) => {
+    set({ preferences: preferences, preferencesLoaded: true });
+    setCookie('preferences', JSON.stringify(preferences), 999);
+  },
 }));
 
 export default usePreferenceStore;
