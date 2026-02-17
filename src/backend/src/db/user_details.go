481cb9b5a102073d44d99e01680d804afa55ac3f
kurama <paul.zenker.pz@gmail.com>
2026-02-17 16:51:29 +0100

    feat: implemented GetDetails
    #92

--- src/backend/src/db/user_details.go
+++ src/backend/src/db/user_details.go
@@ -1,23 +1,74 @@
 package db
 
 import (
+	"fmt"
+
 	"github.com/KuramaSyu/GoToHell/src/backend/src/api/repositories"
 	"github.com/KuramaSyu/GoToHell/src/backend/src/models"
 )
 
 type IUserDetailsFacade interface {
-	GetDetails(userID models.Snowflake, requestingUserID models.Snowflake) ([]models.GetUserDetailsReply, error)
+	GetDetails(userID models.Snowflake, requestingUserID models.Snowflake) (models.GetUserDetailsReply, error)
 }
 
 type UserDetailsFacade struct {
 	SportRepo         SportRepository
 	UserRepo          UserRepository
 	PersonalGoalsRepo repositories.PersonalGoalsRepository
+	FriendshipRepo    FriendshipRepository
 }
 
-func (f *UserDetailsFacade) GetDetails(userID models.Snowflake, requestingUserID models.Snowflake) ([]models.GetUserDetailsReply, error) {
+func (s *UserDetailsFacade) GetDetails(
+	userID models.Snowflake,
+	requestingUserID models.Snowflake,
+) (models.GetUserDetailsReply, error) {
 
-	return []models.GetUserDetailsReply{}, nil
+	// check if they are friends and exit otherwise
+	statusPositive, _ := s.FriendshipRepo.HavePositiveFriendshipStatus(userID, requestingUserID)
+	if !statusPositive {
+		return models.GetUserDetailsReply{}, fmt.Errorf("There is no friendship between %v and %v", userID, requestingUserID)
+	}
+
+	// get username, discriminator (# value) and avatar
+	userInfo, err := s.UserRepo.GetUserByID(userID)
+	if err != nil {
+		return models.GetUserDetailsReply{}, err
+	}
+
+	// get streaks
+	currentStreak, err := s.SportRepo.GetCurrentStreak(userID)
+	if err != nil {
+		return models.GetUserDetailsReply{}, err
+	}
+	LongestStreak, err := s.SportRepo.GetLongestStreak(userID)
+	if err != nil {
+		return models.GetUserDetailsReply{}, err
+	}
+
+	// get last activities
+	LastActivities, err := s.SportRepo.GetSports([]models.Snowflake{userID}, 50, 0)
+	if err != nil {
+		return models.GetUserDetailsReply{}, err
+	}
+
+	// get goals
+	goals, err := s.PersonalGoalsRepo.FetchByUserID(userID, requestingUserID)
+	if err != nil {
+		return models.GetUserDetailsReply{}, err
+	}
+
+	// build up response
+	details := models.GetUserDetailsReply{
+		ID:             userID,
+		Username:       userInfo.Username,
+		Discriminator:  userInfo.Discriminator,
+		Avatar:         userInfo.Avatar,
+		CurrentStreak:  currentStreak,
+		LongestStreak:  LongestStreak,
+		Goals:          goals,
+		LastActivities: LastActivities,
+	}
+	return details, nil
 }
 
 func NewUserDetailsFacade(
