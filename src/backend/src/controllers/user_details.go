a392db9c85bd95551ba0c9f3a75187a748d42008
kurama <paul.zenker.pz@gmail.com>
2026-02-19 17:31:30 +0100

    feat: finished implementation of user details
    closes #92

--- src/backend/src/controllers/user_details.go
+++ src/backend/src/controllers/user_details.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/KuramaSyu/GoToHell/src/backend/src/db"
 	. "github.com/KuramaSyu/GoToHell/src/backend/src/models"
 	"github.com/gin-gonic/gin"
@@ -12,6 +14,9 @@ type UserDetailsData struct {
 	Amount    int           `json:"amount"`
 	Frequency TimeFrequency `json:"frequency"`
 	Sport     string        `json:"sport"`
+}
+
+type GetUserDetailsQuery struct {
 }
 
 func NewPersonalDetailsController(repo db.IUserDetailsFacade) *PersonalDetailsController {
@@ -33,5 +38,26 @@ type PersonalDetailsController struct {
 // @Failure 400 {object} ErrorReply
 // @Router /{user_id}/details [get]
 func (self *PersonalDetailsController) Get(c *gin.Context) {
+	// var query GetUserDetailsReply
 
+	// if err := c.ShouldBindQuery(&query); err != nil {
+	// 	SetGinError(c, http.StatusBadRequest, err)
+	// }
+	requesting_user, status, err := UserFromSession(c)
+	if err != nil {
+		SetGinError(c, status, err)
+		return
+	}
+	requested_user_id, err := NewSnowflakeFromString(c.Param("user_id"))
+	if err != nil {
+		SetGinError(c, http.StatusBadRequest, err)
+		return
+	}
+
+	reply, err := self.repo.GetDetails(requested_user_id, requesting_user.ID)
+	if err != nil {
+		SetGinError(c, http.StatusForbidden, err)
+		return
+	}
+	c.JSON(http.StatusOK, reply)
 }
