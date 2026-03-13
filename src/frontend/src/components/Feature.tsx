fa3ba45f485d0ed838f8661ab4c9a255601fcc1a
kurama <paul.zenker.pz@gmail.com>
2026-03-13 18:43:49 +0100

    feat: implemented Feature Component which wraps feature check
    #112

---
+++ src/frontend/src/components/Feature.tsx
@@ -0,0 +1,18 @@
+// Feature.tsx
+import React, { ReactNode } from 'react';
+import useFeatureStore, { FeatureFlagName } from '../zustand/FeatureStore';
+
+interface FeatureProps {
+  name: FeatureFlagName; // nur existierende Flags
+  children: ReactNode;
+  fallback?: ReactNode; // optionaler Fallback
+}
+
+export const Feature: React.FC<FeatureProps> = ({
+  name,
+  children,
+  fallback = null,
+}) => {
+  const flag = useFeatureStore((state) => state.flags[name]);
+  return <>{flag.enabled ? children : fallback}</>;
+};
