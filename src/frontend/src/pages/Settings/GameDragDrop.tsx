4a7778ffcffbbcee7fe484a29f19c797c160b8ed
kurama <paul.zenker.pz@gmail.com>
2026-04-08 12:25:55 +0200

    chore: cleaned up imports

--- src/frontend/src/pages/Settings/GameDragDrop.tsx
+++ src/frontend/src/pages/Settings/GameDragDrop.tsx
@@ -1,24 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import {
-  DndContext,
-  closestCenter,
-  DragEndEvent,
-  useSensor,
-  useSensors,
-  PointerSensor,
-  DragOverlay,
-} from '@dnd-kit/core';
-import {
-  arrayMove,
-  SortableContext,
-  useSortable,
-  verticalListSortingStrategy,
-} from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
-import { Paper, Box, Typography, alpha } from '@mui/material';
-import { useSportResponseStore } from '../../zustand/sportResponseStore';
-import zIndex from '@mui/material/styles/zIndex';
-import { getThemeNames, useThemeStore } from '../../zustand/useThemeStore';
+import { useState } from 'react';
+import { getThemeNames } from '../../zustand/useThemeStore';
 import { TwoListDnD } from './TwoListDnD';
 import usePreferenceStore from '../../zustand/PreferenceStore';
 import { UserPreferences } from '../../models/Preferences';
