9b0168b3793dc75f97bf6567053d23dc2b68cbb7
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-04-23 01:19:46 +0200

    feat,api: added get_name to description provider

--- src/frontend/src/utils/DescriptionProvider.tsx
+++ src/frontend/src/utils/DescriptionProvider.tsx
@@ -3,26 +3,44 @@ import { Timedelta, unitToString } from './Timedelta';
 
 interface SportDescriptionProvider {
   get_supported_sports(): string[];
-  get_description(computedValue: number): string | undefined;
+  get_description(computedValue: number, sport: string): string | undefined;
   is_supported(sport: string): boolean;
+  get_name(sport_id: string): string;
 }
 
 abstract class SportDescriptionProviderABC implements SportDescriptionProvider {
   abstract get_supported_sports(): string[];
-  abstract get_description(computedValue: number): string | undefined;
+  abstract get_description(
+    computedValue: number,
+    sport: string,
+  ): string | undefined;
   is_supported(sport: string): boolean {
     return this.get_supported_sports().includes(sport);
   }
+  get_name(sport_id: string): string {
+    return GameSelectionMap.get(sport_id) ?? sport_id;
+  }
 }
+
 class TimeDescriptionProvider extends SportDescriptionProviderABC {
   get_supported_sports(): string[] {
     return ['plank', 'workout'];
   }
 
-  get_description(computedValue: number): string | undefined {
+  get_description(computedValue: number, sport: string): string | undefined {
     const timedelta = new Timedelta(computedValue);
     const biggestUnit = timedelta.biggestUnit();
-    return unitToString(biggestUnit);
+    return `${this.get_name(sport)} ${unitToString(biggestUnit)}`;
+  }
+}
+
+class DefaultDescriptionProvider extends SportDescriptionProviderABC {
+  get_supported_sports(): string[] {
+    return [];
+  }
+
+  get_description(computedValue: number, sport: string): string | undefined {
+    return `${this.get_name(sport)}`;
   }
 }
 
@@ -33,14 +51,17 @@ class TimeDescriptionProvider extends SportDescriptionProviderABC {
  */
 export function getSportDescription(
   sport: string | undefined,
-  computedValue: number
+  computedValue: number,
 ): string | undefined {
-  let providers: SportDescriptionProvider[] = [new TimeDescriptionProvider()];
+  let providers: SportDescriptionProvider[] = [
+    new TimeDescriptionProvider(),
+    new DefaultDescriptionProvider(),
+  ];
   for (const provider of providers) {
     if (sport !== undefined && provider.is_supported(sport)) {
-      return provider.get_description(computedValue);
+      return provider.get_description(computedValue, sport);
     }
   }
   if (sport === undefined) return;
-  return GameSelectionMap.get(sport) as string;
+  return new DefaultDescriptionProvider().get_description(computedValue, sport);
 }
