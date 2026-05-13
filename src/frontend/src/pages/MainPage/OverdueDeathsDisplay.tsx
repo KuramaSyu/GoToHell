d59d64323f2d0e1ae4779dbbd71a9834bc369797
kurama <paul.zenker.pz@gmail.com>
2026-05-13 12:48:58 +0200

    ux,ui: added lockIcon and lockDecrement to OverdueDeathsDisplay

--- src/frontend/src/pages/MainPage/OverdueDeathsDisplay.tsx
+++ src/frontend/src/pages/MainPage/OverdueDeathsDisplay.tsx
@@ -1,10 +1,12 @@
-import { alpha, Box, darken, Stack, Typography } from '@mui/material';
+import { alpha, Box, darken, Fade, Stack, Typography } from '@mui/material';
+import Lock from '@mui/icons-material/Lock';
+import LockOpen from '@mui/icons-material/LockOpen';
 import { useThemeStore } from '../../zustand/useThemeStore';
 import { PopNumber } from './PopNumber';
 import { NUMBER_FONT } from '../../statics';
 import { blendWithContrast } from '../../utils/blendWithContrast';
 import { useOverdueDeathsStore } from '../../zustand/OverdueDeathsStore';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {
   ApiRequirement,
   ApiRequirementsBuilder,
@@ -13,7 +15,17 @@ import { hexToRgbString } from '../../utils/colors/hexToRgb';
 
 export const OverdueDeathsDisplay: React.FC = () => {
   const { theme } = useThemeStore();
-  const { overdueDeathsList } = useOverdueDeathsStore();
+  const { overdueDeathsList, lockDecrement, setLockDecrement } =
+    useOverdueDeathsStore();
+
+  // whether or not the display is hovered
+  const [isHovered, setIsHovered] = useState(false);
+
+  // state which is for 5s true, after lockDecrement changed
+  const [showAfterChange, setShowAfterChange] = useState(false);
+
+  // timer which is used to set showAfterChange back to false after 5s
+  const hideTimerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
   useEffect(() => {
     async function init() {
@@ -25,21 +37,102 @@ export const OverdueDeathsDisplay: React.FC = () => {
     init();
   }, []);
 
+  /**
+   * useEffect with timer, used to display the lock icon
+   * 5s after a change to lockDecrement
+   */
+  useEffect(() => {
+    setShowAfterChange(true);
+
+    if (hideTimerRef.current) {
+      clearTimeout(hideTimerRef.current);
+    }
+
+    hideTimerRef.current = setTimeout(() => {
+      setShowAfterChange(false);
+      hideTimerRef.current = null;
+    }, 5000);
+
+    return () => {
+      if (hideTimerRef.current) {
+        clearTimeout(hideTimerRef.current);
+        hideTimerRef.current = null;
+      }
+    };
+  }, [lockDecrement]);
+
+  /**
+   * When the user clicks the overdue deaths display,
+   * toggle the lock decrement option, which makes it so that uploading
+   * a death does not decrement the overdue deaths count.
+   * This should also grey out the display and add a lock icon in the top middle
+   */
+  const onToggleDecrementLock = () => {
+    // toggle lock decrement
+    setLockDecrement(!lockDecrement);
+  };
+
+  const showLockIcon = lockDecrement || isHovered || showAfterChange;
+
   return (
     <Stack direction={'row'} spacing={2} height={'100%'} alignItems={'center'}>
       <Box
         sx={{
+          ...theme.colorTransition.root,
           px: 2,
           py: 1,
-          backgroundColor: theme.blendAgainstContrast('secondary', 0.4),
+
+          // normal colorful background when unlocked, greyed out when locked
+          backgroundColor: !lockDecrement
+            ? theme.blendAgainstContrast('secondary', 0.4)
+            : theme.changeSaturation(
+                theme.blendAgainstContrast('secondary', 0.4),
+                -0.9,
+              ),
           borderRadius: '40px',
           display: 'flex',
-          // alignItems: 'center',
-          // justifyContent: 'center',
           flexDirection: 'column',
+          position: 'relative',
         }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       >
+        <Fade in={showLockIcon} timeout={theme.transitions.duration.complex}>
+          <Box
+            sx={{
+              ...theme.colorTransition.root,
+
+              // middle positioned at the top of the display
+              position: 'absolute',
+              top: '-10px',
+              left: '50%',
+              transform: 'translateX(-50%)',
+
+              // center icon in middle
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+
+              // size and rounding
+              width: '28px',
+              height: '28px',
+              borderRadius: '999px',
+
+              // color
+              backgroundColor: theme.blendAgainstContrast('secondary', 0.25),
+              color: theme.blendWithContrast('secondary', 0.5),
+              pointerEvents: 'none',
+            }}
+          >
+            {lockDecrement ? (
+              <Lock sx={{ fontSize: '18px' }} />
+            ) : (
+              <LockOpen sx={{ fontSize: '18px' }} />
+            )}
+          </Box>
+        </Fade>
         <Box
+          onClick={onToggleDecrementLock}
           sx={{
             display: 'flex',
             overflow: 'hidden',
